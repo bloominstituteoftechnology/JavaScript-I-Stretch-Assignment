d0c2428c8c8d3c69204d53a9b6ac33e39bb23fca
SunJieMing <bennelson@Bens-MacBook-Pro.local>
2017-10-02 12:01:34 -0700

    Updated instructions

--- src/callbacks.js
+++ src/callbacks.js
@@ -50,7 +50,7 @@ sumNums(5, 10, (sum) => {
   console.log(`The sum is ${sum}.`);
 });
 
-// Write a function called multiplyNums that adds two numbers and passes the result to the callback
+// Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
 // code here
 
 multiplyNums(5, 10, (product) => {
