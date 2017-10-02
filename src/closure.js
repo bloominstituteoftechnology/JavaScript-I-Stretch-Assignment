09d94b8791f1b5974cfb09ef589c6a3b6ecc4bcb
SunJieMing <bennelson@Bens-MacBook-Pro.local>
2017-10-02 07:37:57 -0700

    Initial commit

--- src/closure.js
+++ src/closure.js
@@ -4,43 +4,13 @@ const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
-  // newCounter(); // 2 
-  // in the parent function add a counter variable
-  // make new function
-  // return ++? inside function
-  // return function
-  let myCounter = 0;
-  const myFunction = () => {
-    return ++myCounter;
-  }
-return myFunction;
+  // newCounter(); // 2
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-  // create an object
-  // create two methods for the object Inc and Dec
-  // create a counter variable in higher scope
-  // increment will increment a var 
-  //   return inc
-  // decrement will dec a var
-  //  return dex
-  // return object
-  let counter = 0;
-  let myObject = {
-    const increment = () => {
-      counter++;
-      return increment;
-    }
-    const decrement = () => {
-      counter--;
-      return decrement;
-    } 
-        
-  }
-  return myObject;
 };
 
 const limitFunctionCallCount = (cb, n) => {
@@ -48,8 +18,7 @@ const limitFunctionCallCount = (cb, n) => {
   // The returned function should only allow `cb` to be invoked `n` times.
 };
 
-/* STRETCH PROBLEM */
-
+/* Extra Credit */
 const cacheFunction = (cb) => {
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
--- src/objects.js
+++ src/objects.js
@@ -5,48 +5,23 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
-  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
-  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-//   const myMap = {};
-//   const key = Object.keys(obj);
-//   for (let i = 0; i < obj.length; i++) {
-
-//     myMap.push(obj[i]);
-//   }
-//   cb(myMap);
-// };
-
-// Object.keys(obj).map(function(key, index) {
-//   obj[key] *= 2;
-// });
-
-// +  const newObj = {};
-// +  const key = Object.keys(obj);
-// +  for (let i = 0; i < key.length; i++) {
-// +    const newValue = cb(obj[key[i]]);
-// +    newObj[key[i]] = newValue;
-// +  }
-// +  return newObj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-  return Object.entries(obj);
 };
-
-/* STRETCH PROBLEMS */
 
 const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
