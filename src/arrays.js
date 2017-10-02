d525648a5970f8cb21eb9c1e7ab0ad32db00d80c
SunJieMing <bennelson@Bens-MacBook-Pro.local>
2017-10-02 07:53:51 -0700

    Fixed solution that was altered in git conflict

--- src/arrays.js
+++ src/arrays.js
@@ -22,7 +22,7 @@ const map = (elements, cb) => {
   return mappedArr;
 };
 
-const reduce = (elements, cb, startingValue) => {
+const reduce = (elements, cb, startingValue = elements.shift()) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
