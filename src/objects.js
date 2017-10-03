fd16f44e7f695b878570a678fab68ffcd3945755
David Long <davdaarn@live.com>
2017-10-03 17:05:49 -0500

    objects.js

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,39 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  const keyArr = [];
+  Object.keys(obj).forEach((key) => {
+    keyArr.push(key);
+  });
+  return keyArr;
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const objValues = [];
+  Object.keys(obj).forEach(key => objValues.push(obj[key]));
+  return objValues;
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  Object.keys(obj).forEach((key) => {
+    obj[key] = cb(obj[key]);
+  });
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  const pairArr = [];
+  Object.keys(obj).forEach((key) => {
+    pairArr.push([key, obj[key]]);
+  });
+  return pairArr;
 };
 
 /* Extra credit */
@@ -29,12 +46,23 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const invertObj = {};
+  Object.keys(obj).forEach((key) => {
+    invertObj[obj[key]] = key;
+  });
+  return invertObj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  Object.keys(defaultProps).forEach((key) => {
+    if (!obj[key]) {
+      obj[key] = defaultProps[key];
+    }
+  });
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
