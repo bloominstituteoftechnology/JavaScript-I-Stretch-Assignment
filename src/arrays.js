7cfedbea1e263bd50f29850d7f2553eac6a2b637
Tyler Sanford <tyler03sanford@gmail.com>
2017-10-03 17:34:36 -0600

    Finished Homework

--- src/arrays.js
+++ src/arrays.js
@@ -9,28 +9,71 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+
+  const newArray = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(cb(elements[i]));
+  }
+
+  return newArray;
 };
 
 const reduce = (elements, cb, startingValue) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let i = 0;
+
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+    i = 1;
+  }
+
+  let memo = startingValue;
+
+  for (;i < elements.length; i++) {
+    memo = cb(memo, elements[i], i);
+  }
+
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
+
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const newArray = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      newArray.push(elements[i]);
+    }
+  }
+
+  return newArray;
 };
 
 /* Extra Credit */
@@ -38,6 +81,10 @@ const filter = (elements, cb) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+
+  return elements.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
 };
 
 /* eslint-enable no-unused-vars, max-len */
--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,30 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+
+  return Object.keys(obj).map(key => obj[key]);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  Object.keys(obj).forEach(key => (obj[key] = cb(obj[key])));
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+
+  return Object.entries(obj);
 };
 
 /* Extra credit */
@@ -29,12 +37,22 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const newObj = {};
+  Object.keys(obj).forEach(key => (newObj[obj[key]] = key));
+  return newObj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  const map = Object.keys(defaultProps);
+  map.forEach((val) => {
+    if (!obj[val]) {
+      obj[val] = defaultProps[val];
+    }
+  });
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
