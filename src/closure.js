6d09a3172573b91d8ceb48054888d5314b835656
ubuntu <irishseanvaldivia@gmail.com>
2017-10-04 05:12:15 -0500

    Did the best I could

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,37 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  const cb = () => {
+    count += 1;
+    return count;
+  };
+  return cb;
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  const counterObj = {};
+  counterObj.increment = () => {
+    count += 1;
+    return count;
+  };
+  counterObj.decrement = () => {
+    count -= 1;
+    return count;
+  };
+  return counterObj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  const limit = () => {
+  };
+  return limit;
 };
 
 /* Extra Credit */
@@ -27,6 +47,17 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  const invokeCb = () => {
+    Object.keys(cache).forEach((key) => {
+      if (typeof cache[cb] === 'undefined') {
+        cache[cb] = cb();
+        return cache[cb];
+      }
+      return cb();
+    });
+  };
+  return invokeCb;
 };
 
 /* eslint-enable no-unused-vars */
--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,43 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  const newArr = Object.keys(obj);
+  return newArr;
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const newArr = [];
+  Object.keys(obj).forEach((key) => {
+    newArr.push(obj[key]);
+  });
+  return newArr;
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    newObj[key] = cb(obj[key]);
+  });
+  return newObj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  // gonna try to come back and make this one more precise...
+  const newArr = [];
+  Object.keys(obj).forEach((key) => {
+    const innerArr = [];
+    innerArr.push(key);
+    innerArr.push(obj[key]);
+    newArr.push(innerArr);
+  });
+  return newArr;
 };
 
 /* Extra credit */
@@ -29,12 +50,23 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const inverseObj = {};
+  Object.keys(obj).forEach((key) => {
+    inverseObj[obj[key]] = key;
+  });
+  return inverseObj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  Object.keys(defaultProps).forEach((key) => {
+    if (typeof obj[key] === 'undefined') {
+      obj[key] = defaultProps[key];
+    }
+  });
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
