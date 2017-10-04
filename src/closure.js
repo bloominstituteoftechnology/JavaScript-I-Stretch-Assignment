cf8ac0941fe13695a205badd58718f8235472334
Nicholas Mckinley <32463744+That1dude08@users.noreply.github.com>
2017-10-04 16:05:38 -0500

    Update closure.js

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,35 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let yolo = 0;
+    return () => {
+      return ++yolo;
+    };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment: () => count++,
+    decrement: () => count--,
+  }
+  return count;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return {
+    if (count = n) {
+      count++;
+      return (cb);
+    }
+  }
+
 };
 
 /* Extra Credit */
