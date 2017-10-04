6925857c49b0ed2f742bece5ea80986feab3127d
Biff <thedonnieutley@gmail.com>
2017-10-04 13:57:36 -0500

    Initial commit, cacheFunction in closure.js is not done.

--- src/callbacks.js
+++ src/callbacks.js
@@ -20,10 +20,13 @@
  *
  */
 
+const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
+
 // Write a function called firstItem that passes the first item of the given array to the callback function
 // code here
-
-const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
+const firstItem = (arr, cb) => {
+  cb(arr[0]);
+}
 
 firstItem(foods, (firstItem) => {
   console.log(`The first item is ${firstItem}.`);
@@ -31,6 +34,11 @@ firstItem(foods, (firstItem) => {
 
 // Write a function called getLength that passes the length of the array into the callback
 // code here
+const getLength = (arr, cb) => {
+  let length = 0;
+  for (i in arr) { length++; }
+  cb(length);
+}
 
 getLength(foods, (length) => {
   console.log(`The length of the array is ${length}.`);
@@ -38,6 +46,11 @@ getLength(foods, (length) => {
 
 // Write a function called last which passes the last item of the array into the callback
 // code here
+const last = (arr, cb) => {
+  let length = 0;
+  for (i in arr) { length++; }
+  cb(arr[length-1]);
+}
 
 last(foods, (lastItem) => {
   console.log(`The last item in the array is ${lastItem}.`);
@@ -45,6 +58,9 @@ last(foods, (lastItem) => {
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 // code here
+const sumNums = (num1, num2, cb) => {
+  cb(num1+num2);
+}
 
 sumNums(5, 10, (sum) => {
   console.log(`The sum is ${sum}.`);
@@ -52,6 +68,9 @@ sumNums(5, 10, (sum) => {
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 // code here
+const multiplyNums = (num1, num2, cb) => {
+  cb(num1*num2);
+}
 
 multiplyNums(5, 10, (product) => {
   console.log(`The product is ${product}.`);
@@ -60,6 +79,16 @@ multiplyNums(5, 10, (product) => {
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
 // code here
+const contains = (arr, item, cb) => {
+  let isPresent = false;
+  for (i in arr) {
+    if (item === arr[i]) { 
+      isPresent = true;
+      break;
+     }
+  }
+  cb(isPresent);
+}
 
 contains(foods, 'ribeye', (result) => {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
@@ -68,6 +97,18 @@ contains(foods, 'ribeye', (result) => {
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 // code here
+const removeDuplicates = (arr, cb) => {
+  const newArr = [];
+  
+  for (i in arr) {
+    let isPresent = false;
+    for (j in newArr) {
+      if (newArr[j] === arr[i]) { isPresent = true; }
+    }
+    if (!isPresent) { newArr.push(arr[i]); }
+  }
+  cb(newArr);
+}
 
 removeDuplicates(foods, (uniqueFoods) => {
   console.log(`foods with duplicates removed: ${uniqueFoods}`);
@@ -75,6 +116,9 @@ removeDuplicates(foods, (uniqueFoods) => {
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 // code here
+const forEach = (arr, cb) => {
+  for (i in arr) { cb(arr[i], i); }
+}
 
 forEach(foods, (value, index) => {
   console.log(`${value} is at index ${index}.`);
--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,39 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const valuesArr = [];
+  Object.keys(obj).forEach((key) => {
+    valuesArr.push(obj[key]);
+  });
+  return valuesArr;
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    newObj[key] = cb(obj[key]);
+  });
+  return newObj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  // const arr = [];
+  const tmpArr = [];
+  Object.keys(obj).forEach((key) => {
+    tmpArr.push([key, obj[key]]);
+  });
+  return tmpArr;
 };
 
 /* Extra credit */
@@ -29,12 +46,25 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const newObj = {};
+  Object.keys(obj).forEach((key) => {
+    newObj[obj[key]] = key;
+  });
+  return newObj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  if (defaultProps) {
+    Object.keys(defaultProps).forEach((key) => {
+      if (!obj[key]) {
+        obj[key] = defaultProps[key];
+      }
+    });
+  }
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
