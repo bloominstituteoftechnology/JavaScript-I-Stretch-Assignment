c8a804ad106bb1ccc3ebc3cd81f4f0e26bdb8e66
Kia Choi <sgchoi529@email.arizona.edu>
2017-10-04 12:37:29 -0700

    initial commit - finish extra credit

--- src/closure.js
+++ src/closure.js
@@ -49,6 +49,13 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (args) => {
+    if (!(args in cache)) {
+      cache[args] = cb(args);
+    }
+    return cache[args];
+  };
 };
 
 /* eslint-enable no-unused-vars */
