7eaffabd7c05983077a46fa0a33686e9d93b392b
alexfigliolia <alexfigliolia@gmail.com>
2017-10-04 11:23:29 -0400

    hw

--- src/callbacks.js
+++ src/callbacks.js
@@ -25,81 +25,77 @@
 
 const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
-firstItem(foods, (firstItem) => {
-  console.log(`The first item is ${firstItem}.`);
-});
-
 const firstItem = (arr, cb) => {
 	cb(arr[0]);
 }
 
+firstItem(foods, (firstItem) => {
+  console.log(`The first item is ${firstItem}.`);
+});
+
 // Write a function called getLength that passes the length of the array into the callback
 // code here
-
-getLength(foods, (length) => {
-  console.log(`The length of the array is ${length}.`);
-});
 
 const getLength = (arr, cb) => {
 	let len = arr.length;
 	cb(len);
 }
 
+getLength(foods, (length) => {
+  console.log(`The length of the array is ${length}.`);
+});
+
 // Write a function called last which passes the last item of the array into the callback
 // code here
-
-last(foods, (lastItem) => {
-  console.log(`The last item in the array is ${lastItem}.`);
-});
 
 const last = (arr, cb) => {
 	cb(arr[arr.length - 1]);
 }
 
+last(foods, (lastItem) => {
+  console.log(`The last item in the array is ${lastItem}.`);
+});
+
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 // code here
-
-sumNums(5, 10, (sum) => {
-  console.log(`The sum is ${sum}.`);
-});
 
 const sumNums = (num1, num2, cb) => {
 	let sum = num1 + num2;
 	cb(sum);
 }
 
+sumNums(5, 10, (sum) => {
+  console.log(`The sum is ${sum}.`);
+});
+
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 // code here
-
-multiplyNums(5, 10, (product) => {
-  console.log(`The product is ${product}.`);
-});
 
 const multiplyNums = (num1, num2, cb) => {
 	let pr = num1 * num2;
 	cb(pr);
 }
 
+multiplyNums(5, 10, (product) => {
+  console.log(`The product is ${product}.`);
+});
+
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
 // code here
-
-contains(foods, 'ribeye', (result) => {
-  console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
-});
 
 const contains = (arr, item, cb) => {
 	let bool = arr.indexOf(item) > 0;
 	cb(bool);
 }
 
+contains(foods, 'ribeye', (result) => {
+  console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
+});
+
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 // code here
-
-removeDuplicates(foods, (uniqueFoods) => {
-  console.log(`foods with duplicates removed: ${uniqueFoods}`);
-});
 
 const removeDuplicates = (arr, cb) => {
 	let unique = [];
@@ -109,17 +105,21 @@ const removeDuplicates = (arr, cb) => {
 	cb(unique);
 }
 
+removeDuplicates(foods, (uniqueFoods) => {
+  console.log(`foods with duplicates removed: ${uniqueFoods}`);
+});
+
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 // code here
-
-forEach(foods, (value, index) => {
-  console.log(`${value} is at index ${index}.`);
-});
 
 const forEach = (arr, cb) => {
 	arr.forEach((item, i) => {
 		cb(item, i);
 	});
 }
+
+forEach(foods, (value, index) => {
+  console.log(`${value} is at index ${index}.`);
+});
 
 /* eslint-enable */
