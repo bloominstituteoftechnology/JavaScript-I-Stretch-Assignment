af1a45e6f281b9c26a7f81c5439355db855a9dc2
Nicholas Mckinley <32463744+That1dude08@users.noreply.github.com>
2017-10-04 16:05:07 -0500

    Update callbacks.js

--- src/callbacks.js
+++ src/callbacks.js
@@ -21,7 +21,9 @@
  */
 
 // Write a function called firstItem that passes the first item of the given array to the callback function
-// code here
+const firstItem = (foods, yolo) => {
+   yolo(foods[0]);
+};
 
 const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
@@ -30,28 +32,37 @@ firstItem(foods, (firstItem) => {
 });
 
 // Write a function called getLength that passes the length of the array into the callback
-// code here
+const getLength = (foods, yolo) => {
+   yolo(foods.length);
+};
 
 getLength(foods, (length) => {
   console.log(`The length of the array is ${length}.`);
 });
 
 // Write a function called last which passes the last item of the array into the callback
-// code here
+const last = (foods, yolo) => {
+   yolo(foods[foods.length -1]);
+};
 
 last(foods, (lastItem) => {
   console.log(`The last item in the array is ${lastItem}.`);
 });
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
-// code here
+const sumNums = (x, y, yolo) => {
+   yolo( x + y);
+};
 
 sumNums(5, 10, (sum) => {
   console.log(`The sum is ${sum}.`);
 });
 
 // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
-// code here
+const multiplyNums = (x, y, yolo) => {
+   yolo(x * y);
+};
+
 
 multiplyNums(5, 10, (product) => {
   console.log(`The product is ${product}.`);
@@ -59,7 +70,9 @@ multiplyNums(5, 10, (product) => {
 
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
-// code here
+const contains = (foods, item, yolo) => {
+  return yolo(foods.indexOf(item) >= 0);
+};
 
 contains(foods, 'ribeye', (result) => {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
@@ -67,14 +80,20 @@ contains(foods, 'ribeye', (result) => {
 
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
-// code here
+const removeDuplicates = (foods, yolo) => {
+  yolo(foods.filter((food,i) => {
+    return foods.indexOf(food) === i;
+  }));
+};
 
 removeDuplicates(foods, (uniqueFoods) => {
   console.log(`foods with duplicates removed: ${uniqueFoods}`);
 });
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
-// code here
+const forEach = (foods, yolo) => {
+  return foods.forEach((food, i) => yolo(food, i));
+};
 
 forEach(foods, (value, index) => {
   console.log(`${value} is at index ${index}.`);
