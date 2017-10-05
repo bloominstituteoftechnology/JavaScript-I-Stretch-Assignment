69d6fe4a6227fd9229402c17c92f719a4c73d160
BonnW <pwonghansa@gmail.com>
2017-10-05 17:02:59 -0500

    Redone using peer pull requests. for studying purposes ONLY

--- src/callbacks.js
+++ src/callbacks.js
@@ -22,6 +22,9 @@
 
 // Write a function called firstItem that passes the first item of the given array to the callback function
 // code here
+const firstItem = (elements, cb) => {
+  cb(elements[0]);
+}
 
 const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
@@ -31,6 +34,9 @@ firstItem(foods, (firstItem) => {
 
 // Write a function called getLength that passes the length of the array into the callback
 // code here
+const getLength = (elements, cb) => {
+  cb(elements.length);
+}
 
 getLength(foods, (length) => {
   console.log(`The length of the array is ${length}.`);
@@ -45,6 +51,8 @@ last(foods, (lastItem) => {
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 // code here
+const sumNums = (x, y, cb);
+cb(x + y);
 
 sumNums(5, 10, (sum) => {
   console.log(`The sum is ${sum}.`);
@@ -52,6 +60,9 @@ sumNums(5, 10, (sum) => {
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 // code here
+const multiplyNums = (x, y, cb) => {
+  cb(x * y);
+}
 
 multiplyNums(5, 10, (product) => {
   console.log(`The product is ${product}.`);
@@ -60,6 +71,9 @@ multiplyNums(5, 10, (product) => {
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
 // code here
+const contains = (elements, x, eb) => {
+  cb(elements.includes(x));
+}
 
 contains(foods, 'ribeye', (result) => {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
@@ -68,6 +82,15 @@ contains(foods, 'ribeye', (result) => {
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 // code here
+removeDuplicates = (elements, cb) => {
+  let nElements = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (!nElements.includes(elements[i])) {
+      nElements.push(foods[i]);
+    };
+  }
+  cb(nElements);
+}
 
 removeDuplicates(foods, (uniqueFoods) => {
   console.log(`foods with duplicates removed: ${uniqueFoods}`);
@@ -75,6 +98,11 @@ removeDuplicates(foods, (uniqueFoods) => {
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 // code here
+forEach = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  };
+}
 
 forEach(foods, (value, index) => {
   console.log(`${value} is at index ${index}.`);
--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,35 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let x = 0;
+  return () => {
+    return ++x;
+  };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let x = 0;
+  return {
+    increment: () => { return ++x; },
+    decrement: () => { return --x; },
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let nCount = 0;
+
+  return (...args) => {
+    nCount++;
+    if (nCount < n) {
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 /* Extra Credit */
@@ -27,6 +45,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+
+  return (args) => {
+    if (!(args in cache)) {
+      cache[args] = cb(args);
+    }
+    return cache[args];
+  };
 };
 
 /* eslint-enable no-unused-vars */
