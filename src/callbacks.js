0813e5e2edf077fb5803284134f50a0aa6b17c00
Jeff <admin@etisdew.website>
2017-11-06 13:56:25 -0800

    getLength && last

--- src/callbacks.js
+++ src/callbacks.js
@@ -31,16 +31,19 @@ const last = (arr, cb) => {
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-};
+  return cb(x + y);
+}; sumNums(1, 2, item => console.log(item));
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-};
+  return cb(x * y);
+}; multiplyNums(1, 2, item => console.log(item));
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-};
+  return cb();
+}; contains(4, [1,2,3,4], _item => console.log(_item));
 
 /* STRETCH PROBLEM */
 
