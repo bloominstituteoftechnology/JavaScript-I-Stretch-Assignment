e86791760954a9463de32ea850a92b92af8fb34a
Jesse Hood <jessenoob@gmail.com>
2017-11-06 17:13:36 -0500

    Solved all callback problems except for the stretch problem

--- src/callbacks.js
+++ src/callbacks.js
@@ -17,15 +17,25 @@ const last = (arr, cb) => {
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let isInList = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      isInList = true;
+      break;
+    }
+  }
+  cb(isInList);
 };
 
 /* STRETCH PROBLEM */
