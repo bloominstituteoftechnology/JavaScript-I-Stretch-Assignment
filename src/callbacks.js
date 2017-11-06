5345c66de84eb04207974fa2dddef82c0ab95ce6
Roarka <designerexpert@gmail.com>
2017-11-06 16:50:55 -0500

    Added a few results

--- src/callbacks.js
+++ src/callbacks.js
@@ -31,15 +31,24 @@ const last = (arr, cb) => {
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y)
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let result = false;
+  list.forEach(function(element) {
+    if (element === item) {
+      result = true;
+    }
+  });
+  return cb(result);
 };
 
 /* STRETCH PROBLEM */
@@ -47,7 +56,8 @@ const contains = (item, list, cb) => {
 const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  // Do not mutate the original array.git s
+  
 };
 
 /* eslint-enable */
