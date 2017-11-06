b51d3efab35082b076b3de879291b70a8dd3c01a
Abby Tiffany <abby.tiffany@gmail.com>
2017-11-06 15:14:12 -0800

    add code for removeDuplicates

--- src/callbacks.js
+++ src/callbacks.js
@@ -37,6 +37,13 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const finalArr= [];
+  for (let i = 0; i < array.length; i++){
+    if ((finalArr.indexOf(array[i])) === -1){
+      finalArr.push(array[i]);
+    }
+  }
+  cb(finalArr);
 };
 
 /* eslint-enable */
