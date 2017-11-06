513e1c093b3df7916118b59ef9aecc6d40aa1120
Jeff <admin@etisdew.website>
2017-11-06 13:27:16 -0800

    getLength && last

--- src/callbacks.js
+++ src/callbacks.js
@@ -5,14 +5,29 @@ const firstItem = (arr, cb) => {
   cb(arr[0]);
 }; firstItem([1,2,3], item => console.log(item));
 
+// getLength is immutable by creating and destroying a copy of the original array.
+// I selected a while loop in order to reimpliment the functionality of .length.
+
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
-};
+  const _arr = arr.slice();
+  let closure = 0;
+  while (_arr) {
+    closure += 1;
+    _arr.pop();
+    if (_arr[0] === undefined) {
+      break;
+    }
+  }
+  return cb(closure);
+}; getLength([1,2,3], item => console.log(item));
+
+// last, would have been impossible without being able to leverage getLength.
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
-
-};
+  cb(arr[getLength(arr, item => item) - 1]);
+}; last([1,2,4], item => console.log(item));
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
