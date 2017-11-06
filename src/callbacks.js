54011b413a0039e84ac99bc2c8cab847df827fcf
Abby Tiffany <abby.tiffany@gmail.com>
2017-11-06 14:42:56 -0800

    wrote code for contains function

--- src/callbacks.js
+++ src/callbacks.js
@@ -17,15 +17,18 @@ const last = (arr, cb) => {
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.indexOf(item) !== -1);
 };
 
 /* STRETCH PROBLEM */
