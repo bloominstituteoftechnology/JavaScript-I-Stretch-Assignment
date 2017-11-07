5600e98c78345eee13c00b48c2f4ce6ce40e53ad
Jesse Hood <jessenoob@gmail.com>
2017-11-07 18:22:16 -0500

    Completed cacheFunction stretch problem in closure.js, skipped flatten in arrays.js after trying forever to solve it

--- src/closure.js
+++ src/closure.js
@@ -40,6 +40,15 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (arg) => {
+    if (arg in cache) {
+      return cache[arg];
+    }
+    const result = cb(arg);
+    cache[arg] = result;
+    return result;
+  };
 };
 
 /* eslint-enable no-unused-vars */
