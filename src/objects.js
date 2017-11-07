798ca3f6824de2c81616d533fa4d16160a633791
Jesse Hood <jessenoob@gmail.com>
2017-11-06 19:42:49 -0500

    solved invert stretch problem in object.js

--- src/objects.js
+++ src/objects.js
@@ -48,6 +48,12 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const newObj = {};
+  const objKeys = Object.keys(obj);
+  for (let i = 0; i < objKeys.length; i++) {
+    newObj[obj[objKeys[i]]] = objKeys[i];
+  }
+  return newObj;
 };
 
 const defaults = (obj, defaultProps) => {
