076bd5e5ebd3c205d116b55ade1af201b4a3f210
Roarka <designerexpert@gmail.com>
2017-11-06 19:49:15 -0500

    progress in arrays

--- src/arrays.js
+++ src/arrays.js
@@ -9,19 +9,38 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mapArr = [];
+  each(elements, (element) => {
+    mapArr.push(cb(element));
+  });
+  return mapArr;
 };
 
 const reduce = (elements, cb, startingValue) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
-  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  // `starting-Value` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  const reduced = [];
+  let initVal = 0;
+  if (startingValue) {
+    initVal = startingValue;
+  }
+
+  for (let i = 0; i < elements.length; i++) {
+    cb(initVal, elements[i]);
+  }
+  initVal;
 };
+  // reduce([1,2,3,4],(ourParam)=> console.log(ourParam));
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
