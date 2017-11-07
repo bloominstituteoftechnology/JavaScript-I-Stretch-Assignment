eeb1d9b0e7d68d0100a568a7203f30c48a391749
KickMe <kickme@alifs-mbp.gateway.sonic.net>
2017-11-06 16:32:03 -0800

    Working on callbacks.js

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,43 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
+
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+c
+  if (list[i] == item) {
+    cb(true);
+  }
+  else {
+    cb(false);
+  }
+}
+
 };
 
 /* STRETCH PROBLEM */
@@ -31,6 +47,16 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+for (let i = 0; i < array.length; i++) {
+  for (let k = i + 1; k < array[i]; k++) {
+
+  }
+
+}
+
+let newArray
+
 };
 
 /* eslint-enable */
