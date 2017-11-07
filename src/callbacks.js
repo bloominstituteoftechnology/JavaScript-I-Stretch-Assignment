0f6e9ebdcc5afd9d34dd4ec01604d730dde4b5ea
Vinnie <vinnie.scalco@gmail.com>
2017-11-06 18:09:26 -0600

    first five callback

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,22 +2,27 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
@@ -31,6 +36,13 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let obj = [];
+  for(let i=0; i < array.length; i++)
+  {
+    if(obj.indexOf(array[i]) === -1)
+      obj.push(array[i]);
+  }  
+  cb(obj);
 };
 
 /* eslint-enable */
