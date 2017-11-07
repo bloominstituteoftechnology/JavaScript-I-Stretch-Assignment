4f38fa445f0aa650c2c1b9a3195055f0bcd0a07f
Sarika <sarikaresume@gmail.com>
2017-11-07 14:10:25 -0800

    hello ricky

--- src/objects.js
+++ src/objects.js
@@ -5,23 +5,35 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
-};
+};  return Object.keys(obj);
+};		  };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
-};
+};return Object.values(obj);
+};		  };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-};
+};const newObj = {};
++  for (let i = 0; i < Object.keys(obj).length; i++) {
++    newObj[Object.keys(obj)[i]] = cb(Object.values(obj)[i]);
++  }
++  return newObj;
+ };		  };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-};
+};const newArr = [];
++  for (let i = 0; i < Object.keys(obj).length; i++) {
++    newArr.push([Object.keys(obj)[i], Object.values(obj)[i]]);
++  }
++  return newArr;
+ };		  };
 
 /* STRETCH PROBLEMS */
 
@@ -29,14 +41,25 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
-};
+}; const newObj = {};
++  for (let i = 0; i < Object.keys(obj).length; i++) {
++    newObj[Object.values(obj)[i]] = Object.keys(obj)[i];
++  }
++  return newObj;
+ };		  };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
 };
-
+for (let i = 0; i < Object.keys(defaultProps).length; i++) {
+  +    if (!obj[Object.keys(defaultProps)[i]]) {
+  +      obj[Object.keys(defaultProps)[i]] = Object.values(defaultProps)[i];
+  +    }
+  +  }
+  +  return obj;
+   };		  };
 /* eslint-enable no-unused-vars */
 
 module.exports = {
