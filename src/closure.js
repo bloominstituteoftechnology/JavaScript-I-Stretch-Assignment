4a9d9fec88d9454931c9a618bbcfd2cda13be932
Vinnie <vinnie.scalco@gmail.com>
2017-11-07 13:12:41 -0600

    more closure

--- src/closure.js
+++ src/closure.js
@@ -7,8 +7,10 @@ const counter = () => {
   // newCounter(); // 2
   let count = 0;
   return () => {
-    return ++count;
+    count++;
+    return count;
   };
+};
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
@@ -19,6 +21,12 @@ const counterFactory = () => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) return null;
+    callCount++;
+    return cb(...args);
+  }
 };
 
 /* STRETCH PROBLEM */
