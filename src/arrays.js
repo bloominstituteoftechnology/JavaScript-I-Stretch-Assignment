5de5c2fdde02a80e66f70a91d3b65852076304c8
Adam Fetters <31798262+adamcodesjava@users.noreply.github.com>
2017-11-06 22:19:42 -0500

    All but reduce and Stretch functions completed in Arrays

--- src/arrays.js
+++ src/arrays.js
@@ -29,17 +29,35 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  if (startingValue !== undefined) {
+    startingValue = elements[0];
+  }
+  for (let i = 0; i < elements.length; i++)  {
+    cb(startingValue, elements[i]);
+  }
+
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) return elements[i];
+  }
+
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filtered = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) filtered.push(elements[i]);
+  }
+
+  return filtered;
 };
 
 /* STRETCH PROBLEM */
