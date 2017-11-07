6bed30b3576ce69c1b120ee84c03aa5b98638432
Roarka <designerexpert@gmail.com>
2017-11-07 12:40:03 -0500

    removing console logs

--- src/callbacks.js
+++ src/callbacks.js
@@ -3,7 +3,7 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
   cb(arr[0]);
-}; firstItem([1,2,3], item => console.log(item));
+};
 
 // getLength is immutable by creating and destroying a copy of the original array.
 // I selected a while loop in order to reimpliment the functionality of .length.
@@ -20,24 +20,24 @@ const getLength = (arr, cb) => {
     }
   }
   return cb(closure);
-}; getLength([1,2,3], item => console.log(item));
+};
 
 // last, would have been impossible without being able to leverage getLength.
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
   return cb(arr[getLength(arr, item => item) - 1]);
-}; last([1,2,4], item => console.log(item));
+};
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x + y);
-}; sumNums(1, 2, item => console.log(item));
+};
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x * y);
-}; multiplyNums(1, 2, item => console.log(item));
+};
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
@@ -49,7 +49,7 @@ const contains = (item, list, cb) => {
       }
   };
   return cb(result);
-}; contains(5, [1,2,3,4], _item => console.log(_item));
+};
 
 /* STRETCH PROBLEM */
 
@@ -59,7 +59,7 @@ const removeDuplicates = (arr, cb) => {
   const _arr = new Set(arr);
   const __arr = new Array(..._arr);  
   return cb(__arr);
-}; removeDuplicates([1,1,2,3,4], _item => console.log(_item));
+};
 
 /* eslint-enable */
 module.exports = {
