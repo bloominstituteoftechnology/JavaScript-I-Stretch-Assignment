a36a2d89ae88fcb77d64da3fef5d476a9dc2a00e
Ting Wang <felix.wang.1026@gmail.com>
2017-11-07 13:48:31 -0500

    fix the limitFunctionCallCount question

--- src/closure.js
+++ src/closure.js
@@ -32,7 +32,7 @@ const limitFunctionCallCount = (cb, n) => {
   // The returned function should only allow `cb` to be invoked `n` times.
   let count = 0;
   return (...args) => {
-    if (count) {
+    if (count === n) {
       return null;
     }
     count++;
