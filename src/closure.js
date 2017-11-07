139121c501d56f75405e084871f9b15ec05599f4
fairlight-excalibur <lidgrt@gmail.com>
2017-11-07 17:24:43 -0600

    Javascript I HW Jessica Thomas

--- src/closure.js
+++ src/closure.js
@@ -1,19 +1,42 @@
 // Complete the following functions.
 
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let currentCount = 0;
+return function () {
+  return ++currentCount;
+};
+  // Return a function that when invoked increments and returns a counter letiable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
 };
 
 const counterFactory = () => {
+  let count = 0;
+  const counterObject = {};
+  counterObject.count = 0;
+  counterObject.increment = function () {
+      return ++count;
+    };
+  counterObject.decrement = function () {
+      return --count;
+    };
+  return counterObject;
   // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  // `increment` should increment a counter letiable in closure scope and return it.
+  // `decrement` should decrement the counter letiable and return it.
 };
 
 const limitFunctionCallCount = (cb, n) => {
+  let callCount = 0;
+  return (...args) => {
+    callCount++;
+    if (callCount <= n) {
+      return cb(...args);
+    }
+  return null;
+  };
+  return funcCall;
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
@@ -21,15 +44,29 @@ const limitFunctionCallCount = (cb, n) => {
 /* STRETCH PROBLEM */
 
 const cacheFunction = (cb) => {
+  const cacheObject = {
+    cachedArgs: [],
+    results: [],
+  };
+  return (args) => {
+    for (let i = 0; i < cacheObject.cachedArgs.length; i++) {
+      if (cacheObject.cachedArgs.includes(args)) {
+        return cacheObject.results[cacheObject.cachedArgs.indexOf(args)];
+      }
+    }
+    cacheObject.results.push(cb(args));
+    cacheObject.cachedArgs.push(args);
+    return cacheObject.results[cacheObject.results.length - 1];
+  };
+};
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
-};
 
-/* eslint-enable no-unused-vars */
+/* eslint-enable no-unused-lets */
 
 module.exports = {
   counter,
--- src/objects.js
+++ src/objects.js
@@ -2,23 +2,48 @@
 // Reference http://underscorejs.org/ for examples.
 
 const keys = (obj) => {
+  const keyList = Array();
+  for (const prop in obj) {
+  keyList.push(prop);
+}
+  return keyList;
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
 };
 
 const values = (obj) => {
+  const valueList = Array();
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+	  valueList.push(obj[prop]);
+}
+  }
+  return valueList;
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
 };
 
 const mapObject = (obj, cb) => {
+  const newObj = obj;
+  for (const prop in newObj) {
+	  newObj[prop] = cb(obj[prop]);
+  }
+  return newObj;
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
 };
 
 const pairs = (obj) => {
+  const pairList = Array();
+  let keyVal = Array(2);
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+	  keyVal = [prop, obj[prop]];
+	  pairList.push(keyVal);
+}
+  } return pairList;
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
 };
@@ -26,18 +51,33 @@ const pairs = (obj) => {
 /* STRETCH PROBLEMS */
 
 const invert = (obj) => {
+  const invertedObj = {};
+  for (const prop in obj) {
+	  invertedObj[obj[prop]] = prop;
+  }
+  return invertedObj;
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
 };
 
 const defaults = (obj, defaultProps) => {
+  const newObj = {};
+  const listOfKeys = Object.keys(defaultProps);
+  for (let i = 0; i < listOfKeys.length; i++) {
+    if (obj[listOfKeys[i]] === undefined || !obj.hasOwnProperty(listOfKeys[i])) {
+      newObj[listOfKeys[i]] = defaultProps[listOfKeys[i]];
+    } else {
+      newObj[listOfKeys[i]] = obj[listOfKeys[i]];
+    }
+  }
+  return newObj;
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
 };
 
-/* eslint-enable no-unused-vars */
+/* eslint-enable no-unused-lets */
 
 module.exports = {
   keys,
