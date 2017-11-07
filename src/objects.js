4cb16059977f179436ff15a54d4e7ea457d273b5
Aaron Wilder <element1028@gmail.com>
2017-11-07 14:59:39 -0800

    finished objects.js

--- src/objects.js
+++ src/objects.js
@@ -30,8 +30,8 @@ const mapObject = (obj, cb) => {
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-  const keyArr = Object.keys(obj);
-  const valArr = Object.values(obj);
+  const keyArr = keys(obj);
+  const valArr = values(obj);
 
   let newArray = [];
   const bigArray = [];
@@ -49,12 +49,27 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const keyArr = keys(obj);
+  const valArr = values(obj);
+  const newObj = {};
+  for (let i = 0; i < keyArr.length; i++) {
+    newObj[valArr[i]] = keyArr[i];
+  }
+  return newObj;
 };
+
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  const defaultKeys = keys(defaultProps);
+  for (let i = 0; i < defaultKeys.length; i++) {
+    if (obj[defaultKeys[i]] === undefined) {
+      obj[defaultKeys[i]] = defaultProps[defaultKeys[i]];
+    }
+  }
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
