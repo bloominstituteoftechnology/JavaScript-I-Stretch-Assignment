46ee9a1f0239104880c819644c81eb2a80a3237f
Ting Wang <felix.wang.1026@gmail.com>
2017-11-07 01:02:46 -0500

    Modify the limitFunctionCallCount function, still not working

--- src/closure.js
+++ src/closure.js
@@ -30,7 +30,7 @@ const counterFactory = () => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
-  let i = 1;
+  let i = 0;
   return (...args) => {
     while (i <= n) {
       i++;
