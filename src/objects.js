b0e8690ea6e40edf8f02f84fcb2e8e6de0dacbc5
Mark Marchant <jtla3@yahoo.com>
2017-11-07 15:50:54 -0800

    my bad

--- src/objects.js
+++ src/objects.js
@@ -15,8 +15,15 @@ const values = (obj) => {
 const mapObject = (obj, cb) => {
     // Like map for arrays, but for objects. Transform the value of each property in turn.
     // http://underscorejs.org/#mapObject
+  const mapped = {};
 
+  const keyPair = Object.keys(obj);
+  for (let i = 0; i < keyPair.length; i++) {
+    mapped[keyPair[i]] = cb(obj[keyPair[i]]);
+  }
+  return mapped;
 };
+
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
@@ -30,12 +37,24 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const inverted = {};
+
+  const keyPair = Object.keys(obj);
+  for (let i = 0; i < keyPair.length; i++) {
+    inverted[obj[keyPair[i]]] = keyPair[i];
+  }
+
+  return inverted;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  Object.keys(defaultProps).forEach((key) => {
+    obj[key] = obj[key] || defaultProps[key];
+  });
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
