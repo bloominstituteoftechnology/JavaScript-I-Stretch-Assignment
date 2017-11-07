3186d8d12f3dc947154112e8f2ffe8b7283854a2
Vinnie <vinnie.scalco@gmail.com>
2017-11-07 16:48:18 -0600

    all except objects

--- src/objects.js
+++ src/objects.js
@@ -4,7 +4,7 @@
 const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
-  // Based on http://underscorejs.org/#keys
+  // Based on http://underscorejs.org/#keys 
 };
 
 const values = (obj) => {
