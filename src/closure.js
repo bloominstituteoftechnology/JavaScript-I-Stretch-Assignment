7a158eaa5a8e01f0a9e62e360ce4f5f69158d145
Stephanie <sbowl001@fiu.edu>
2017-11-08 07:53:09 +0800

    one problem left

--- src/closure.js
+++ src/closure.js
@@ -14,7 +14,17 @@ const counter = () => {
 };
 
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
+  let counter = 0;
+  return { 
+    increment: () => {
+      return ++counter;
+    },
+    decrement: () => {
+      return --counter;
+    }
+
+  };
+   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
@@ -32,9 +42,9 @@ const limitFunctionCallCount = (cb, n) => {
 /* STRETCH PROBLEM */
 
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
+  return cb()// Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
+  ...args // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
--- src/objects.js
+++ src/objects.js
@@ -2,37 +2,57 @@
 // Reference http://underscorejs.org/ for examples.
 
 const keys = (obj) => {
+    return Object.keys(obj); 
+    
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
 };
 
 const values = (obj) => {
-  // Return all of the values of the object's own properties.
+  return Object.values(obj);// Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
 };
 
 const mapObject = (obj, cb) => {
-  // Like map for arrays, but for objects. Transform the value of each property in turn.
-  // http://underscorejs.org/#mapObject
-};
+ /* for ( let i = 0; i < Object.keys(obj).length; i++) {
+    obj[key[i]] = cb(obj[key[i]]);
+  }// Like map for arrays, but for objects. Transform the value of each property in turn.
+  return obj;// http://underscorejs.org/#mapObject
+}; */  
+  Object.keys(obj).forEach(key => (obj[key] = cb(obj[key])));
+  return obj;  
+};  
 
 const pairs = (obj) => {
-  // Convert an object into a list of [key, value] pairs.
+  return Object.entries(obj);// Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
 };
 
 /* STRETCH PROBLEMS */
 
 const invert = (obj) => {
+ // dream world - return values(keys) switched into keys(values).join('');
+    const keyArr = keys(obj);
+    const valArr = values(obj);
+    const newObj = {};
+    for ( let i = 0; i < keyArr.length; i++) {
+      newObj[valArr[i]] = keyArr[i];
+    }
+    return newObj;
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
 };
 
 const defaults = (obj, defaultProps) => {
-  // Fill in undefined properties that match properties on the `defaultProps` parameter object.
+  const defaultKeys = keys(defaultProps);
+  for (let i = 0; i < defaultKeys.length; i++) {
+    if (obj[defaultKeys[i]] === undefined) {
+      obj[defaultKeys[i]] = defaultProps[defaultKeys[i]];
+    }
+  } return obj;// Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
 };
