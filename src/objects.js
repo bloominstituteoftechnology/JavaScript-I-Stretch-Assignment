bf7e6b885dd9bfd50e7bdcafb7ddb2664a68f9b8
Nicole Phillips <nphillips78@gmail.com>
2017-11-07 16:43:47 -0500

    objects completed

--- src/objects.js
+++ src/objects.js
@@ -2,40 +2,74 @@
 // Reference http://underscorejs.org/ for examples.
 
 const keys = (obj) => {
+  return Object.keys(obj).slice();
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
 };
 
 const values = (obj) => {
+  return Object.values(obj);
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
 };
 
 const mapObject = (obj, cb) => {
-  // Like map for arrays, but for objects. Transform the value of each property in turn.
-  // http://underscorejs.org/#mapObject
+  const newObj = {};
+  const objKeys = keys(obj);
+  for (let i = 0; i < objKeys.length; i++) {
+    const key = objKeys[i];
+    newObj[key] = cb(obj[key]);
+  }
+  return newObj;
 };
+// Like map for arrays, but for objects. Transform the value of each property in turn.
+// http://underscorejs.org/#mapObject
 
 const pairs = (obj) => {
-  // Convert an object into a list of [key, value] pairs.
-  // http://underscorejs.org/#pairs
+  const keyA = Object.keys(obj);
+  const valA = Object.values(obj);
+  let hulk = [];
+  const cap = [];
+  for (let i = 0; i < keyA.length; i++) {
+    hulk.push(keyA[i], valA[i]);
+    cap.push(hulk);
+    hulk = [];
+  }
+  return cap;
 };
+
+// Convert an object into a list of [key, value] pairs.
+// http://underscorejs.org/#pairs
 
 /* STRETCH PROBLEMS */
 
 const invert = (obj) => {
-  // Returns a copy of the object where the keys have become the values and the values the keys.
-  // Assume that all of the object's values will be unique and string serializable.
-  // http://underscorejs.org/#invert
+  const keyA = Object.keys(obj);
+  const valA = Object.values(obj);
+  const hawkeye = {};
+  for (let i = 0; i < keyA.length; i++) {
+    hawkeye[valA[i]] = keyA[i];
+  }
+  return hawkeye;
 };
+// Returns a copy of the object where the keys have become the values and the values the keys.
+// Assume that all of the object's values will be unique and string serializable.
+// http://underscorejs.org/#invert
 
 const defaults = (obj, defaultProps) => {
-  // Fill in undefined properties that match properties on the `defaultProps` parameter object.
-  // Return `obj`.
-  // http://underscorejs.org/#defaults
+  const defaultKeys = keys(defaultProps);
+  for (let i = 0; i < defaultKeys.length; i++) {
+    if (obj[defaultKeys[i]] === undefined) {
+      obj[defaultKeys[i]] = defaultProps[defaultKeys[i]];
+    }
+  }
+  return obj;
 };
+// Fill in undefined properties that match properties on the `defaultProps` parameter object.
+// Return `obj`.
+// http://underscorejs.org/#defaults
 
 /* eslint-enable no-unused-vars */
 
