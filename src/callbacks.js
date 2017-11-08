f963e5941e51ee115eb0528abe75721871089aaa
Aaron Wilder <element1028@gmail.com>
2017-11-08 08:20:33 -0800

    used a ternary operator instead of if else

--- src/callbacks.js
+++ src/callbacks.js
@@ -29,11 +29,7 @@ const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   for (let i = 0; i < list.length; i++) {
-    if( list[i] === item ) {
-      cb(true);
-    } else {
-      cb(false);
-    }
+    list[i] === item ? cb(true) : cb(false);
   }
 };
 
