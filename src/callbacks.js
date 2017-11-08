18b91ffd852222a5fbc83a0482f65c9e109fbcfd
William Antony <32254679+williampathadan@users.noreply.github.com>
2017-11-08 11:48:48 -0600

    Completed callbacks.js

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,42 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let listContainsItem = false;
+
+  for(let i=0; i<=list.length-1; i++){
+    if(list[i] === item){
+      listContainsItem = true;
+      break;
+    }
+  }
+
+  cb(listContainsItem);
 };
 
 /* STRETCH PROBLEM */
@@ -31,6 +46,27 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let newArray = [];
+  for(let i=0; i<=array.length-1; i++){    
+
+    let isUniqueItem = true;
+
+    for(let e=0; e<=newArray.length-1; e++){
+
+      if(array[i] === newArray[e]){
+        isUniqueItem = false;
+        break;
+      }
+
+    }
+
+    if(isUniqueItem === true){
+      newArray.push(array[i]);
+    }
+  }
+
+  cb(newArray);
+  
 };
 
 /* eslint-enable */
