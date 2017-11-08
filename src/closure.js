4f1bebb8c6bcdd637728a7cffdbf0e453bbc43a2
Adam Fetters <31798262+adamcodesjava@users.noreply.github.com>
2017-11-07 20:47:18 -0500

    All tests pass - Completed

--- src/closure.js
+++ src/closure.js
@@ -49,6 +49,13 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return function (x) {
+    if (!(x in cache)) {
+      cache[x] = cb(x);
+    }
+    return cache[x];
+  };
 };
 
 /* eslint-enable no-unused-vars */
