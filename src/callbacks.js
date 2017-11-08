7e4f492c3a471066717af3b0240aae4a0c896297
FedRamirez <33363348+FedRamirez@users.noreply.github.com>
2017-11-08 16:37:38 -0500

    Semi-Complete

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,26 +1,36 @@
 /* eslint-disable */
 
 const firstItem = (arr, cb) => {
+  cb(arr[0]);
   // firstItem passes the first item of the given array to the callback function.
 };
 
 const getLength = (arr, cb) => {
+  cb(arr.length);
   // getLength passes the length of the array into the callback.
 };
 
 const last = (arr, cb) => {
+  cb(arr[arr.length-1]);
   // last passes the last item of the array into the callback.
 };
 
 const sumNums = (x, y, cb) => {
+  cb(x+y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 };
 
 const multiplyNums = (x, y, cb) => {
+  cb(x*y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 };
 
 const contains = (item, list, cb) => {
+ if(list.includes(item)===true){
+   return cb(true);
+ }else{
+   return cb(false);
+ }
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 };
@@ -28,11 +38,19 @@ const contains = (item, list, cb) => {
 /* STRETCH PROBLEM */
 
 const removeDuplicates = (array, cb) => {
-  // removeDuplicates removes all duplicate values from the given array.
+  let noDuplicates=[];
+  for(let i=0;i<array.length;i++){
+if(noDuplicates.includes(array[i]===false)){
+  noDuplicates.push(array[i]);
+}
+
+  }
+  return noDuplicates;
+  cb(noDuplicates);    
+};
+// removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-};
-
 /* eslint-enable */
 module.exports = {
   firstItem,
