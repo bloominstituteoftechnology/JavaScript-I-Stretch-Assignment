29ebdd564a0eb75667d4a62089de652b3a650f04
cgroov <30381292+groov1234@users.noreply.github.com>
2017-12-05 09:36:53 -0500

    Calum Groover - arrays complete - callbacks missing stretch problem

--- src/arrays.js
+++ src/arrays.js
@@ -9,11 +9,17 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mapArr = [];
+  each(elements, item => (mapArr.push(cb(item))));
+  return mapArr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -21,17 +27,32 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  const elementsCopy = elements.slice();
+  let ele = startingValue || elementsCopy.shift();
+  each(elementsCopy, (item) => {
+    ele = cb(ele, item);
+  });
+  return ele;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) return elements[i];
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filterMe = [];
+  each(elements, (item) => {
+    if (cb(item)) filterMe.push(item);
+  });
+  return filterMe;
 };
 
 /* STRETCH PROBLEM */
@@ -39,6 +60,11 @@ const filter = (elements, cb) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const flat = reduce(elements, (flatter, item) => {
+    if (Array.isArray(item)) return flatter.concat(flatten(item));
+    return flatter.concat(item);
+  }, []);
+  return flat;
 };
 
 /* eslint-enable no-unused-vars, max-len */
--- src/callbacks.js
+++ src/callbacks.js
@@ -12,19 +12,28 @@ const getLength = (arr, cb) => {
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    cb(true)
+  }
+  else {
+    cb(false)
+  }
 };
 
 /* STRETCH PROBLEM */
@@ -33,7 +42,7 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-};
+ };
 
 /* eslint-enable */
 module.exports = {
