fb77c321e8cd8c37732358e5b5104b6bd3d0cfe7
Samuel Cha <sunchipssam@gmail.com>
2017-12-05 17:32:08 -0800

    JavaScript-I Completed Assignment for Samuel Cha

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,38 +1,50 @@
 /* eslint-disable */
 
 const firstItem = (arr, cb) => {
-  // firstItem passes the first item of the given array to the callback function.
   cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
-  // getLength passes the length of the array into the callback.
-  cb(arr.length); // do i have ur code ryan ? ahh
+  cb(arr.length); 
 };
 
 const last = (arr, cb) => {
-  // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 };
 
 const sumNums = (x, y, cb) => {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x+y);
 };
 
 const multiplyNums = (x, y, cb) => {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 };
 
 const contains = (item, list, cb) => {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  let itemIsPresent = false;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      itemIsPresent = true;
+      break;
+    }
+  }
+  cb(itemIsPresent);
 };
 
-/* STRETCH PROBLEM */
-
 const removeDuplicates = (array, cb) => {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  let noDupesArray = [];
+  for (let i = 0; i < array.length; i++) {
+      let isDuplicate = false;
+      for (let j = 0; j < noDupesArray.length; j++) {
+        if (array[i] === noDupesArray[j]) {
+          isDuplicate = true
+        }        
+      }
+      if (!isDuplicate) {
+        noDupesArray.push(array[i])        
+      }
+  }
+  cb(noDupesArray);
 };
 
 /* eslint-enable */
