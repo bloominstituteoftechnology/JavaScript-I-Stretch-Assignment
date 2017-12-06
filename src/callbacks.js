9b0ae58ee66ed2f8cb8918f26d142c25ac52194d
barrettjviatorlambdaschool <31706127+barrettjviatorlambdaschool@users.noreply.github.com>
2017-12-06 09:45:07 -0600

    Attempted to solve tests

--- src/callbacks.js
+++ src/callbacks.js
@@ -12,19 +12,28 @@ const getLength = (arr, cb) => {
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+cb(arr[arr.length -1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(var i = 0; i < list.length; i++ ) {
+    if(list[i] === item)
+      cb(true);
+    else
+      cb(false);
+  }
 };
 
 /* STRETCH PROBLEM */
