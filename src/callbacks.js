5c281c44041dd2cef7e8840c38a3501365a8fb65
Elly <ellyhahn@gmail.com>
2017-12-05 22:42:56 -0800

    first assignment

--- src/callbacks.js
+++ src/callbacks.js
@@ -12,19 +12,29 @@ const getLength = (arr, cb) => {
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length -1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === item) {
+      cb(true);
+      } else {
+      cb(false);
+      }
+    }
 };
 
 /* STRETCH PROBLEM */
@@ -33,7 +43,15 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if(newArr.indexOf(array[i]) === -1) {
+      newArr.push(array[i]);
+    };
+  };
+  return cb(newArr);
 };
+
 
 /* eslint-enable */
 module.exports = {
