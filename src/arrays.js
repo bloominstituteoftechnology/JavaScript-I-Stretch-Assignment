91c76664f5adf30db106a33986cca35dc3fdd4ec
Vic <jusSumGuy@users.noreply.github.com>
2017-12-06 08:29:31 -0800

    Vartan Shamirian

--- src/arrays.js
+++ src/arrays.js
@@ -5,6 +5,9 @@
 // You can use the functions that you have already written to help solve the other problems
 
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
@@ -12,11 +15,27 @@ const each = (elements, cb) => {
 };
 
 const map = (elements, cb) => {
+  const newArray = [];
+  each(elements, (element, index) => {
+    newArray[index] = cb(element);
+  });
+  return newArray;
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
 };
 
 const reduce = (elements, cb, startingValue) => {
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+    for (let i = 1; i < elements.length; i++) {
+      startingValue = cb(startingValue, elements[i]);
+    }
+  } else {
+    for (let i = 0; i < elements.length; i++) {
+      startingValue = cb(startingValue, elements[i]);
+    }
+  }
+  return startingValue;
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
@@ -24,12 +43,23 @@ const reduce = (elements, cb, startingValue) => {
 };
 
 const find = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) return elements[i];
+  }
+  return undefined;
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
 };
 
 const filter = (elements, cb) => {
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      newArr.push(elements[i]);
+    }
+  }
+  return newArr;
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
 };
@@ -37,8 +67,15 @@ const filter = (elements, cb) => {
 /* STRETCH PROBLEM */
 
 const flatten = (elements) => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let outPut = [];
+  each(elements, (num) => {
+    if (Array.isArray(num)) {
+      outPut = outPut.concat(flatten(num));
+    } else {
+      outPut.push(num);
+    }
+  });
+  return outPut;
 };
 
 /* eslint-enable no-unused-vars, max-len */
--- src/objects.js
+++ src/objects.js
@@ -2,23 +2,40 @@
 // Reference http://underscorejs.org/ for examples.
 
 const keys = (obj) => {
+  return Object.keys(obj);
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
 };
 
 const values = (obj) => {
+  return Object.values(obj);
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
 };
 
 const mapObject = (obj, cb) => {
+  const newOb = {};
+  const ObKeys = Object.keys(obj);
+  const ObVal = Object.values(obj);
+  for (let i = 0; i < ObKeys.length; i++) {
+    newOb[ObKeys[i]] = cb(ObVal[i]);
+  }
+  return newOb;
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
 };
 
 const pairs = (obj) => {
+  const pairArr = [];
+  const ObKeys = Object.keys(obj);
+  const ObVal = Object.values(obj);
+  for (let i = 0; i < ObKeys.length; i++) {
+    pairArr.push([ObKeys[i], ObVal[i]]);
+  }
+  return pairArr;
+
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
 };
@@ -26,12 +43,25 @@ const pairs = (obj) => {
 /* STRETCH PROBLEMS */
 
 const invert = (obj) => {
+  const newOb = {};
+  const ObVal = Object.keys(obj);
+  const ObKeys = Object.values(obj);
+  for (let i = 0; i < ObKeys.length; i++) {
+    newOb[ObKeys[i]] = ObVal[i];
+  }
+  return newOb;
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
 };
 
 const defaults = (obj, defaultProps) => {
+  const dPK = Object.keys(defaultProps);
+  const dPV = Object.values(defaultProps);
+  for (let i = 0; i < dPK.length; i++) {
+    if (!(dPK[i] in obj)) obj[dPK[i]] = dPV[i];
+  }
+  return obj;
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
