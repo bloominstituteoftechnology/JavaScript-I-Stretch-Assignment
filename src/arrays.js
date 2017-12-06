579f98f8ec66a8ded27df8ecafd9b8093829adf5
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-12-05 18:54:41 -0600

    completed

--- src/arrays.js
+++ src/arrays.js
@@ -9,29 +9,77 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArr = [];
+  each(elements, (item, index) => {
+    newArr.push(cb(item, index));
+  });
+  return newArr;
 };
 
 const reduce = (elements, cb, startingValue) => {
+  // arr, (memo, num) => (memo + num)
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  // let reduced;
+  // if (startingValue === undefined) {
+  //   reduced = elements[0];
+  //   for (let i = 1; i < elements.length; i++) {
+  //     reduced = cb(reduced, elements[i]);
+  //   }
+  // } else {
+  //   reduced = startingValue;
+  //   for (let i = 0; i < elements.length; i++) {
+  //     reduced = cb(reduced, elements[i]);
+  //   }
+  // }
+  // return reduced;
+  const newElements = elements.slice();
+  if (startingValue === undefined) {
+    startingValue = newElements.shift();
+  }
+  let memo = startingValue;
+  each(newElements, (el) => {
+    memo = cb(memo, el);
+  });
+  return memo;
 };
+
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  let result;
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      result = elements[i];
+    } else {
+      result = undefined;
+    }
+  }
+  return result;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      newArr.push(elements[i]);
+    }
+  }
+  return newArr;
 };
 
 /* STRETCH PROBLEM */
@@ -39,8 +87,23 @@ const filter = (elements, cb) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let outPut = [];
+  each(elements, (num) => {
+    if (Array.isArray(num)) {
+      outPut = outPut.concat(flatten(num));
+    } else {
+      outPut.push(num);
+    }
+  });
+  // for (let i = 0; i < elements.length; i++) {
+  //   for (let j = 0; j < elements[i].length; i++) {
+  //     newArr.push(elements[i][j]);
+  //   }
+  // }
+  return outPut;
 };
-
+// const myArr = [1, [2], [3, [[4]]]];
+// console.log(flatten(myArr));
 /* eslint-enable no-unused-vars, max-len */
 
 module.exports = {
