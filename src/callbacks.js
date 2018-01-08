912364bc7d5f322ed63f5b5f117dad838417dba8
cliffkang <cliffgkang@gmail.com>
2018-01-08 14:36:21 -0800

    finished callbacks

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,35 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1])
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let result = false;
+  if (list.indexOf(item) >= 0) result = true;
+  cb(result);
 };
 
 /* STRETCH PROBLEM */
@@ -31,6 +39,16 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const arr = [];
+  const obj = {};
+  array.forEach((x) => {
+    if (obj[x] === 1) return 
+    else {
+      obj[x] = 1;
+      arr.push(x);
+    }
+  });
+  cb(arr);
 };
 
 /* eslint-enable */
