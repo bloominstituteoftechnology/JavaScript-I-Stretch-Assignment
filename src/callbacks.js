ae323a8aecc359c7c8d6fd45964b8cd9646d0183
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-01-08 18:41:30 -0500

    Initial commit

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,23 +1,30 @@
 /* eslint-disable */
 
 const firstItem = (arr, cb) => {
-  // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
+
+firstItem(['first', 'second', 'third'], (firstItem) => {
+  console.log(firstItem);
+});
+
 const getLength = (arr, cb) => {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
-  // last passes the last item of the array into the callback.
+  cb(arr.slice(-1)[0]);// last passes the last item of the array into the callback.
 };
 
 const sumNums = (x, y, cb) => {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+ const sum = x + y;
+ cb(sum);// sumNums adds two numbers (x, y) and passes the result to the callback.
 };
 
 const multiplyNums = (x, y, cb) => {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  const product = x * y;
+  cb(product);// multiplyNums multiplies two numbers and passes the result to the callback.
 };
 
 const contains = (item, list, cb) => {
