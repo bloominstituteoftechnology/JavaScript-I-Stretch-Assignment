042cff6aa3ce724b49a582f9cdfb0bd99a4bcea0
Giraud Julemis <giraud.julemis@gmail.com>
2018-01-08 17:13:14 -0500

    solved each and map functions

--- src/arrays.js
+++ src/arrays.js
@@ -9,11 +9,19 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(cb(elements[i]));
+  }
+  return newArray;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -21,12 +29,22 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+  }
+  for (let i = 0; i < elements.length; i++) {
+    cb(startingValue, elements[i]);
+  }
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i]);
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
