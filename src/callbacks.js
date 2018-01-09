2f176a07d348216c0b847a3270244917aa4b3590
Giraud Julemis <giraud.julemis@gmail.com>
2018-01-08 23:23:23 -0500

    completed callback.js

--- src/callbacks.js
+++ src/callbacks.js
@@ -12,19 +12,27 @@ const getLength = (arr, cb) => {
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const sum = x + y;
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const product = x * y;
+  cb(product);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    cb(item === list[i]);
+  }
 };
 
 /* STRETCH PROBLEM */
@@ -33,6 +41,13 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const duplicate = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== array[i + 1] && !duplicate.includes(array[i])) {
+      duplicate.push(array[i]);
+    }
+  }
+  cb(duplicate);
 };
 
 /* eslint-enable */
