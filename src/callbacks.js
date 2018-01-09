c513e1d72c8113d29e82ac9b779803755baddbc0
Mephestys <mephestys@gmail.com>
2018-01-08 21:39:50 -0700

    arrays.js and callbacks.js completed

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,26 +1,32 @@
 /* eslint-disable */
 
 const firstItem = (arr, cb) => {
+  cb(arr[0]);
   // firstItem passes the first item of the given array to the callback function.
 };
 
 const getLength = (arr, cb) => {
+  cb(arr.length);
   // getLength passes the length of the array into the callback.
 };
 
 const last = (arr, cb) => {
+  cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 };
 
 const sumNums = (x, y, cb) => {
+  cb(x + y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 };
 
 const multiplyNums = (x, y, cb) => {
+  cb(x * y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 };
 
 const contains = (item, list, cb) => {
+  cb(list.includes(item))
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 };
@@ -28,6 +34,7 @@ const contains = (item, list, cb) => {
 /* STRETCH PROBLEM */
 
 const removeDuplicates = (array, cb) => {
+  cb([...new Set(array)]);
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
