2139c6bef17dba3946ec113c5afdae99357beb25
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-01-09 18:24:00 -0500

    arraysSectionComplete

--- src/arrays.js
+++ src/arrays.js
@@ -9,38 +9,91 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
-
-const map = (elements, cb) => {
+const map = (elements, cb) => { // Struggled on this one because I was thinking left to right, rather than right to left - needed for mapping/passing into/through functions
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-};
+  const newArray = [];
 
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(cb(elements[i])); // Each value of 'elements' is passed through cb, then passed pushed onto 'newArray'
+  }
+  return newArray;
+};
 const reduce = (elements, cb, startingValue) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
-  // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
-  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
-};
+  // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument. // Immediately indicates that you will have cb(startingValue, elements[i])
+  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value. // Notice "if" -- tipping you off to the likely need for a conditional statement
+  let i = 0;
 
+  if (startingValue === undefined) { // Why is this necessary?
+    startingValue = elements[0];
+    i = 1;
+  }
+
+  let memo = startingValue;
+
+  for (; i < elements.length; i++) {
+    memo = cb(memo, elements[i]); // Satisfies third bullet point.  Each value + corr. index in the 'elements' array is passed through cb, but how are the elements combined? #QUESTION
+  }
+  return memo;
+};
 const find = (elements, cb) => {
-  // Look through each value in `elements` and pass each element to `cb`.
-  // If `cb` returns `true` then return that element.
-  // Return `undefined` if no elements pass the truth test.
+  // Look through each value in `elements` and pass each element to `cb`. // for (let i = 0 ; i < elements.length ; i++) AND cb(elements[i], i)
+  // If `cb` returns `true` then return that element. // If statement
+  // Return `undefined` if no elements pass the truth test. // ''
+  for (let i = 0; i < elements.length; i++) {
+    if ((cb(elements[i])) === true) {
+      return elements[i];
+    } // Careful not to put return statements outside the wrong brackets! #QUESTION -- Why does 'return undefined;' not work here???
+  } return undefined;
 };
 
 const filter = (elements, cb) => {
-  // Similar to `find` but you will return an array of all elements that passed the truth test
+  // Similar to `find` but you will return an array of all elements that passed the truth test // "return an array" - indicates you will write - "const newArray = [];"
   // Return an empty array if no elements pass the truth test
+// INITIAL ATTEMPT:
+//
+// for (let i = 0; i < elements.length; i++) {
+//   if ((cb(elements[i])) === true) {
+//     newArray.push = (elements[i]);
+//     return newArray;
+//   } else if (elements.length === 0) {
+//     return elements;
+//   }
+// };
+// Missing declaration of newArray.  'true' does not need to be expicitely stated as it is implied. 'return newArray' comes AFTER closing bracket of 'if'
+// Lastly, you did not need to tell JS to print the empty 'elements' array because the function will do that anyway, if no elements pass the truth test.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      newArray.push(elements[i]);
+    }
+  } return newArray;
 };
 
 /* STRETCH PROBLEM */
 
 const flatten = (elements) => {
-  // Flattens a nested array (the nesting can be to any depth).
+  // Flattens a nested array (the nesting can be to any depth). // Since this function must work with "any" depth of nesting, a recursive array flattening function is necessary
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-};
 
+  const flat = [];
+  elements.forEach((item) => {
+    if (Array.isArray(item)) { // This uses the Array constructors 'isArray' property which returns true if a variable is an array, and false if not.
+      flat.push(...flatten(item)); // If ^ finds an array, that array must be flattened into individual items, then those items are looped back through the 'flatten' function.
+    } else { // What looks like an ellipsis '...' is called a spread operator (see link below)
+      flat.push(item);// Through recursion, eventually all the arrays will be flattened to a single list of items (with no arrays), and those items will be pushed into one flattened array.
+    } // #QUESTION: How do you know whether a semi-colon is required or not???
+  });
+  return flat;
+};
+// Based on the following discussion thread: https://stackoverflow.com/questions/30048388/javascript-recursive-array-flattening
+//  //On "Spread Operator": https://docs.microsoft.com/en-us/scripting/javascript/reference/spread-operator-decrement-dot-dot-dot-javascript
 /* eslint-enable no-unused-vars, max-len */
 
 module.exports = {
