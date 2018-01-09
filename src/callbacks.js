96cf5b305ab575445c83aed0a80c3a997049e136
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-01-08 20:55:11 -0500

    cleaning up

--- src/callbacks.js
+++ src/callbacks.js
@@ -10,30 +10,27 @@ const getLength = (arr, cb) => {
 };
 
 const last = (arr, cb) => {
-  cb(arr.slice(-1)[0]);// last passes the last item of the array into the callback.
+  cb(arr.slice(-1)[0]);
 };
 
 const sumNums = (x, y, cb) => {
   const sum = x + y;
-  cb(sum);// sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   const product = x * y;
-  cb(product);// multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(product);
 };
 
 const contains = (item, list, cb) => {
-  cb(list.includes(item));// contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 };
 
 /* STRETCH PROBLEM */
 
 const removeDuplicates = (array, cb) => {
-  cb([...new Set(array)]);// can use forEach loop ...removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  cb([...new Set(array)]);
 };
 
 /* eslint-enable */
