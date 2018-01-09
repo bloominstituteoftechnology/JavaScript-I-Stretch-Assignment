a7e146f36107ce26436de1b5c907a9f83ce868c2
jonbrunt <jbrunt.mobile@gmail.com>
2018-01-09 01:54:51 -0600

    Initial commit

--- src/arrays.js
+++ src/arrays.js
@@ -9,11 +9,19 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i += 1) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  for (let i = 0; i < elements.length; i += 1) {
+    arr.push(cb(elements[i]));
+  }
+  return arr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -21,17 +29,39 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let accumulator;
+  let initialIndex;
+  if (startingValue) {
+    accumulator = startingValue;
+    initialIndex = 0;
+  } else {
+    accumulator = elements[0];
+    initialIndex = 1;
+  }
+  for (let i = initialIndex; i < elements.length; i += 1) {
+    accumulator = cb(accumulator, elements[i]);
+  }
+  return accumulator;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i += 1) {
+    if (cb(elements[i])) return elements[i];
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const arr = [];
+  for (let i = 0; i < elements.length; i += 1) {
+    if (cb(elements[i])) arr.push(elements[i]);
+  }
+  return arr;
 };
 
 /* STRETCH PROBLEM */
