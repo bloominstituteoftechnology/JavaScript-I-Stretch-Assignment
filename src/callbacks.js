b1cdfb95b05ff991f4b77b5fea97384ed12aeed8
Jonathan Anderson <jonathananderson@Jonathans-MacBook-Pro.local>
2018-01-08 23:31:54 -0600

    cb and arrays are finished

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,7 +2,7 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
-  cb(arr[0]);
+  return cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
@@ -11,18 +11,27 @@ const getLength = (arr, cb) => {
 };
 
 const last = (arr, cb) => {
+  cb(arr.pop());  
   // last passes the last item of the array into the callback.
 };
 
 const sumNums = (x, y, cb) => {
+  cb(x + y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 };
 
 const multiplyNums = (x, y, cb) => {
+  cb(x * y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 };
 
 const contains = (item, list, cb) => {
+  for (let i=0; i < list.length; i++) {
+    if (item[i] === true) {
+      cb(true);
+    }
+    cb(false);
+  }
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 };
