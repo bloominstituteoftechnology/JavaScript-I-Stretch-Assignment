eec43589978be70c45bb8aff1f7d6474fe6d43a2
ashleijo <shuryjo@gmail.com>
2018-01-09 15:16:42 -0500

    adding completed closure.js assignment.

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,40 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let x = 0;
+  function addOne() {
+    return ++x;
+  }
+  return addOne;
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  const obj = {};
+  let x = 0;
+  obj.increment = function inc() {
+    return ++x;
+  };
+  obj.decrement = function dec() {
+    return --x;
+  };
+  return obj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  function a(...arg) {
+    if (count < n) {
+      count++;
+      return cb(...arg);
+    }
+    return null;
+  }
+  return a;
 };
 
 /* STRETCH PROBLEM */
@@ -27,6 +50,15 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  function a(input) {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) {
+      return cache[input];
+    }
+    cache[input] = cb(input);
+    return cache[input];
+  }
+  return a;
 };
 
 /* eslint-enable no-unused-vars */
