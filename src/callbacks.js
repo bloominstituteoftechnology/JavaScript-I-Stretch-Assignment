3ff49e67539a53cb1122b6390b87c7b63681a51c
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-01-08 22:08:49 -0500

    Finished most of arrays and callbacks; still working on a reduce, flatten and removeDuplicates

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,40 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  const lastIndex = arr.length - 1;
+  cb(arr[lastIndex]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y)
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let isPresent = false;
+  for (let i = 0; i < list.length; i ++) {
+    if (item === list[i]) {
+      isPresent = true;
+    }
+  }
+  return cb(isPresent);
 };
 
 /* STRETCH PROBLEM */
@@ -31,6 +44,13 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let originalArray = array;
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array[i - 1]; i++) {
+      if ()
+    }
+  }
 };
 
 /* eslint-enable */
