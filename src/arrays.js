ab50c35b51771d95f7746c0565399c47e97eeb01
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-01-09 21:22:25 -0500

    Java=1 Commit

--- src/arrays.js
+++ src/arrays.js
@@ -5,6 +5,9 @@
 // You can use the functions that you have already written to help solve the other problems
 
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
@@ -12,24 +15,59 @@ const each = (elements, cb) => {
 };
 
 const map = (elements, cb) => {
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(cb(elements[i]));
+  }
+  return newArray;
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
 };
 
 const reduce = (elements, cb, startingValue) => {
-  // Combine all elements into a single value going from left to right.
+  let memo = startingValue;
+  let i = 0;
+  if (startingValue === undefined) {
+    memo = elements[0];
+    i++;
+  }
+  for (i; i < elements.length; i++) {
+    memo = cb(memo, elements[i]);
+  }
+  return memo;
+
+  // Combine all elements into a single value going from left to right (e.g. 1,2,3,4,5 -> 12345).
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
 };
 
 const find = (elements, cb) => {
+  let boolean = false;
+  for (let i = 0; i < elements.length; i++) {
+    boolean = cb(elements[i]);
+    if (boolean === true) {
+      return elements[i];
+    }
+  }
+  if (boolean === false) {
+    return undefined;
+  }
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
 };
 
 const filter = (elements, cb) => {
+  let boolean = false;
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    boolean = cb(elements[i]);
+    if (boolean === true) {
+      arr.push(elements[i]);
+    }
+  }
+  return arr;
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
 };
@@ -37,6 +75,10 @@ const filter = (elements, cb) => {
 /* STRETCH PROBLEM */
 
 const flatten = (elements) => {
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(elements[i]);
+  }
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
