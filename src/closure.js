2a8dad4959c48faea96c5042e756493194e45295
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-01-10 09:54:05 -0500

    Latest progress on closure.js

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,40 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return () => {
+    return ++count;
+  };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  const inc = () => {
+    return ++count;
+  };
+  const dec = () => {
+    return --count;
+  };
+  const obj = {};
+  obj.increment = inc;
+  obj.decrement = dec;
+  return obj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count < n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 /* STRETCH PROBLEM */
@@ -27,6 +50,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  return (...args) => {
+    const cache = {};
+    cache[args] = cb(...args);
+    if (Object.keys(cache).indexOf(...args) === -1) {
+      return cb(...args);
+    }
+    return cache[args];
+  };
 };
 
 /* eslint-enable no-unused-vars */
