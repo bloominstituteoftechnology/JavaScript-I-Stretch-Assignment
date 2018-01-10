5fcc3212bdbc08b736f62d4aff0426568289cfaf
Lo Saephan <teamsky916@gmail.com>
2018-01-09 22:24:08 -0800

    js-i hw second commit unfishihed

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,45 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+
+  //* loop through each key and push it in array
+  const keyArray = [];
+  Object.keys(obj).forEach((key) => {
+    keyArray.push(key);
+  });
+  return keyArray;
 };
 
+//* loop through each key and push the key value in an array
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const valueArray = [];
+  Object.keys(obj).forEach((key) => {
+    valueArray.push(obj[key]);
+  });
+  return valueArray;
 };
 
+//* loop through each key, invoke cb and pass in key value then assign the return value to current object property
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  Object.keys(obj).forEach((key) => {
+    obj[key] = cb(obj[key]);
+  });
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  const keyValue = [];
+  Object.keys(obj).forEach((key) => {
+    keyValue.push([key, obj[key]]);
+  });
+  return keyValue;
 };
 
 /* STRETCH PROBLEMS */
@@ -29,6 +52,9 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  // Object.keys(obj).forEach((key) => {
+  // });
+
 };
 
 const defaults = (obj, defaultProps) => {
