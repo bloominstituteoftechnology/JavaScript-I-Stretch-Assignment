029ba11be12daea95bf7ed157a38f73efe6d1ffc
ashleijo <shuryjo@gmail.com>
2018-01-10 16:14:34 -0500

    need help with stretch problem

--- src/recursion.js
+++ src/recursion.js
@@ -21,6 +21,18 @@ const nFactorial = (n) => {
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+	if (typeof obj === 'object') {
+			for (let i in Object.keys(obj)) {
+				if (typeof Object.values(obj)[i] !== "object") {
+					console.log(Object.values(obj)[i])
+				}
+				if (typeof Object.values(obj)[i] == "object") {
+					checkMatchingLeaves(Object.values(obj)[i])
+				}
+			}
+		}
+		// right now, I am able to print all the values in the object.
+		// Still trying to figure out how to compare them.
 };
 
 /* eslint-enable no-unused-vars */
