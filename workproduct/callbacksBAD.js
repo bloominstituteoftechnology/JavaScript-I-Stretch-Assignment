626b1a9f710f188f62751ec7b4db9174d80e67c3
Glenn-David Daniel <d2rd@outlook.com>
2018-01-09 17:15:10 -0800

    Using correct modules from src folder

---
+++ workproduct/callbacksBAD.js
@@ -0,0 +1,25 @@
+const listOfFood = [
+    'cheetoes',
+    'bananas',
+    'turkey leg',
+    'cobbler',
+    'bananas',
+  ];
+
+function firstItem {
+  
+}
+let listOfFood[1]
+ For (let i = str.length - 1; i >= 0; i--) {
+reversedString += str(i);
+}
+arr = listOfFood;
+
+const firstItem = (arr, cb) => {
+  cb(arr[0]);
+  // firstItem passes the first item of the given array to the callback function.
+};
+// ---- behind the scenes
+firstItem(['first', 'second', 'third'], (firstItem) => {
+  console.log(firstItem);
+});
