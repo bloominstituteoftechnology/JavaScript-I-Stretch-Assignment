864ab0576e89cdceae294797ce5846c9bf7021d3
temp <temp@Debis-Home-Mac.local>
2018-01-10 01:43:28 -0500

    best attempts

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,43 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length -1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x *n y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (item === list[i]) {
+      return cb(true);    //
+    }                     //Is the return here necessary?
+  } return cb(false);     //
+
+  //Option #2: Trying to use forEach:
+  list.forEach(test => {
+    if (test === item) return cb(true);
+  });
+  return cb(false);  
 };
 
 /* STRETCH PROBLEM */
@@ -31,6 +47,22 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+  const noDuplicates = [];
+  let duplicate = 0;
+  
+  array.forEach(x => {
+    for (let i = 0; i < array.length; i++){
+      if (x === array[i]) {
+        duplicate++;
+      }
+    }
+    if (duplicate < 2) {
+      noDuplicates.push(x);
+    }
+  });
+
+  cb(noDuplicates);
 };
 
 /* eslint-enable */
