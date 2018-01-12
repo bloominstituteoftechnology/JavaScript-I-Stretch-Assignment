4ac0001cfebf3465ea53549599b4e3931e9680ae
John Coronel <john.m.coronel@gmail.com>
2018-01-11 18:17:53 -0800

    First Attempt 90%

--- src/arrays.js
+++ src/arrays.js
@@ -9,11 +9,17 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    arr.push(cb(elements[i]));
+  }
+  return arr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -21,17 +27,38 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let result = startingValue !== undefined ? startingValue : elements[0];
+  let i = startingValue !== undefined ? 0 : 1;
+
+  for (i; i < elements.length; i++) {
+    result = cb(result, elements[i]);
+  }
+  return result;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      return elements[i];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      arr.push(elements[i]);
+    }
+  }
+  return arr;
 };
 
 /* STRETCH PROBLEM */
@@ -51,3 +78,4 @@ module.exports = {
   filter,
   flatten,
 };
+
