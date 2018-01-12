4db2038817f750bde0aac36b1c366adb6f4783e1
Mac <mac@Macs-MBP-4.attlocal.net>
2018-01-11 19:45:44 -0600

    Justin Ruiz initial commit. 50% complete

--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,41 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  //return cb(arr.length);
+  return cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  return cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let product = x * y;
+  return cb(product);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.length; i++) {
+    if(list[i] === item){
+      return cb(true);
+    }
+  }
+  return cb(false);
 };
 
 /* STRETCH PROBLEM */
