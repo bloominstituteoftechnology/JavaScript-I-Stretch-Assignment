e805407365d0774cb6273897898af78803b4b493
Streltz <35007289+Streltz@users.noreply.github.com>
2018-01-13 14:51:43 -0500

    JavaScript-I first attempt

--- src/arrays.js
+++ src/arrays.js
@@ -9,25 +9,18 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
-  for (let i = 0; i<elements.length; i++);{
-    cb(elements[i]);}
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
   const mappedvalues = [];
-  for (let i = 0; i<elements.length; i++){
-    mappedvalues.push(cb(elements[i]));
-  }
+  each(elements, item => (mappedvalues.push(cb(item))));
   return mappedvalues;
 };
 
 const reduce = (elements, cb, startingValue) => {
-  // Combine all elements into a single value going from left to right.
-  // Elements will be passed one by one into `cb` along with the `startingValue`.
-  // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
-  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
   const sum = elements.reduce((runningTotal, num) => runningTotal + num);
   cb(sum);
 };
@@ -35,35 +28,27 @@ const reduce = (elements, cb, startingValue) => {
 const find = (elements, cb) => {
   for (let i = 0; i < elements.length; i++) {
     if (elements[i] === cb) return true;
+    return false;
   }
-  return false;
-  // Look through each value in `elements` and pass each element to `cb`.
-  // If `cb` returns `true` then return that element.
-  // Return `undefined` if no elements pass the truth test.
+};
+const filter = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    map(elements[i] === cb);
+  }
 };
 
-const filter = (elements, cb) => {
-  // Similar to `find` but you will return an array of all elements that passed the truth test
-  // Return an empty array if no elements pass the truth test
+const flatten = (elements) => {
+  let flatArray = [];
   for (let i = 0; i < elements.length; i++) {
-    map (elements[i] === cb);
+    if (Array.isArray(elements[i])) {
+      flatArray = flatArray.concat(flatten(elements[i]));
+    } else {
+      flatArray.push(elements[i]);
+    }
   }
+  return flatArray;
 };
 
 /* STRETCH PROBLEM */
 
-const flatten = (elements) => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-};
-
 /* eslint-enable no-unused-vars, max-len */
-
-module.exports = {
-  each,
-  map,
-  reduce,
-  find,
-  filter,
-  flatten,
-};
--- src/callbacks.js
+++ src/callbacks.js
@@ -1,38 +1,42 @@
 /* eslint-disable */
 
 const firstItem = (arr, cb) => {
-  // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
-  // last passes the last item of the array into the callback.
+  cb(arr[arr.length -1]);
 };
 
 const sumNums = (x, y, cb) => {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-};
+
 
 /* STRETCH PROBLEM */
-
 const removeDuplicates = (array, cb) => {
-  // removeDuplicates removes all duplicate values from the given array.
+    // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-};
-
+  let duplicate = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== array[i + 1] && duplicate.includes(array[i])) {
+      duplicate.push(array[i]);
+    }
+  }
+  cb(duplicate);
+ };	
+  };  
 /* eslint-enable */
 module.exports = {
   firstItem,
@@ -41,5 +45,4 @@ module.exports = {
   sumNums,
   multiplyNums,
   contains,
-  removeDuplicates,
 };
