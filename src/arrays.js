9ccb0d319078d3eca048e95713b80f4ae449a3f5
Streltz <35007289+Streltz@users.noreply.github.com>
2018-01-13 17:07:39 -0500

    JS-I Complete

--- src/arrays.js
+++ src/arrays.js
@@ -15,40 +15,63 @@ const each = (elements, cb) => {
 };
 
 const map = (elements, cb) => {
-  const mappedvalues = [];
-  each(elements, item => (mappedvalues.push(cb(item))));
-  return mappedvalues;
+  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
+  // Return the new array.
+  const mappedArr = [];
+  each(elements, item => (mappedArr.push(cb(item))));
+  return mappedArr;
 };
 
-const reduce = (elements, cb, startingValue) => {
-  const sum = elements.reduce((runningTotal, num) => runningTotal + num);
-  cb(sum);
+const reduce = (elements, cb, startingValue = elements.shift()) => {
+  // Combine all elements into a single value going from left to right.
+  // Elements will be passed one by one into `cb` along with the `startingValue`.
+  // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
+  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let memo = startingValue;
+  each(elements, (item) => {
+    memo = cb(memo, item);
+  });
+  return memo;
 };
 
 const find = (elements, cb) => {
+  // Look through each value in `elements` and pass each element to `cb`.
+  // If `cb` returns `true` then return that element.
+  // Return `undefined` if no elements pass the truth test.
   for (let i = 0; i < elements.length; i++) {
-    if (elements[i] === cb) return true;
-    return false;
+    if (cb(elements[i])) return elements[i];
   }
-};
-const filter = (elements, cb) => {
-  for (let i = 0; i < elements.length; i++) {
-    map(elements[i] === cb);
-  }
+  return undefined;
 };
 
-const flatten = (elements) => {
-  let flatArray = [];
-  for (let i = 0; i < elements.length; i++) {
-    if (Array.isArray(elements[i])) {
-      flatArray = flatArray.concat(flatten(elements[i]));
-    } else {
-      flatArray.push(elements[i]);
-    }
-  }
-  return flatArray;
+const filter = (elements, cb) => {
+  // Similar to `find` but you will return an array of all elements that passed the truth test
+  // Return an empty array if no elements pass the truth test
+  const filteredValues = [];
+  each(elements, (item) => {
+    if (cb(item)) filteredValues.push(item);
+  });
+  return filteredValues;
 };
 
 /* STRETCH PROBLEM */
 
+const flatten = (elements) => {
+  // Flattens a nested array (the nesting can be to any depth).
+  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const flattenedArr = reduce(elements, (memo, item) => {
+    if (Array.isArray(item)) return memo.concat(flatten(item));
+    return memo.concat(item);
+  }, []);
+  return flattenedArr;
+};
 /* eslint-enable no-unused-vars, max-len */
+
+module.exports = {
+  each,
+  map,
+  reduce,
+  find,
+  filter,
+  flatten,
+};
