f399779af892339d3e903f944bcf0f5d48dc7c8b
Alex Myers <amyers88@outlook.com>
2018-01-13 21:37:22 -0700

    Arrays is complete, passing 2/3 test for Stretch Problem "Flatten".

--- src/arrays.js
+++ src/arrays.js
@@ -70,6 +70,8 @@ const filter = (elements, cb) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const newArr = elements.concat.apply([], elements);
+  return newArr;
 };
 
 /* eslint-enable no-unused-vars, max-len */
