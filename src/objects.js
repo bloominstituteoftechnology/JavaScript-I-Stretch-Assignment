19010eb01df8a7d8b52e2bdeffccb7a6c5ffb1d9
Alex Myers <amyers88@outlook.com>
2018-01-13 20:52:02 -0700

    Objects is complete and passing, including Stretch Problems

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,32 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const newObj = {};
+  const key = Object.keys(obj);
+  for (let i = 0; i < key.length; i++) {
+    const newVal = cb(obj[key[i]]);
+    newObj[key[i]] = newVal;
+  }
+  return newObj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 /* STRETCH PROBLEMS */
@@ -29,12 +39,20 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const newObj = {};
+  const ents = Object.entries(obj);
+
+  for (let i = 0; i < ents.length; i++) {
+    newObj[ents[i][1]] = ents[i][0];
+  }
+  return newObj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  return Object.assign(defaultProps, obj);
 };
 
 /* eslint-enable no-unused-vars */
