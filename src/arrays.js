717df33eea3f8ff20f27ecc3b2c721d53b4cc334
Dan-Lara <endtrippy@gmail.com>
2018-01-15 17:31:55 -0700

    J1 Completed

--- src/arrays.js
+++ src/arrays.js
@@ -9,11 +9,19 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const answer = [];
+  for (let i = 0; i < elements.length; i++) {
+    answer.push(cb(elements[i]));
+  }
+  return answer;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -21,17 +29,38 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let i = 0;
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+    i++;
+  }
+  for (;i < elements.length; i++) {
+    startingValue = cb(startingValue, elements[i]);
+  }
+  return startingValue;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const nuArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      nuArr.push(elements[i]);
+    }
+  }
+  return nuArr;
 };
 
 /* STRETCH PROBLEM */
@@ -40,14 +69,17 @@ const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
-
 /* eslint-enable no-unused-vars, max-len */
 
 module.exports = {
   each,
   map,
+  flatten,
   reduce,
   find,
   filter,
-  flatten,
 };
+
+function newFunction(cb, elements, i) {
+  return cb((elements[i]), i);
+}
--- src/callbacks.js
+++ src/callbacks.js
@@ -2,27 +2,39 @@
 
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb((x+y));
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb((x*y));
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.forEach(element => {
+    if (element === item) {
+      cb(true);
+    }
+    else
+      cb(false);
+  });
 };
 
 /* STRETCH PROBLEM */
@@ -31,6 +43,16 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const nuArray = array;
+
+  for (let i = 0; i < nuArray.length; i++) {
+    for (let j = i + 1; j < nuArray.length; j++) {
+      if (nuArray[i] === nuArray[j]) {
+        nuArray.splice(j);
+      }
+    }
+  }
+  cb(nuArray);
 };
 
 /* eslint-enable */
