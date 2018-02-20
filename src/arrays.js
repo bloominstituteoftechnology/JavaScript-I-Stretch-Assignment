8a6ad6b113051ca4c70321e83498a75a0ed5452b
kait-schorr <kaitlynschorr@gmail.com>
2018-02-20 15:54:46 -0600

    Solved find

--- src/arrays.js
+++ src/arrays.js
@@ -50,6 +50,11 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
 };
 
 const filter = (elements, cb) => {
