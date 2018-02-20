3c896eaef5bca1a7cf736b6d5c31e9902435830e
kait-schorr <kaitlynschorr@gmail.com>
2018-02-20 15:53:39 -0600

    Finished reduce function

--- src/arrays.js
+++ src/arrays.js
@@ -36,6 +36,13 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  if (startingValue === undefined) {
+    startingValue = elements.shift();
+  }
+  for (let i = 0; i < elements.length; i++) {
+    startingValue = cb(startingValue, elements[i]);
+  }
+  return startingValue;
 };
 
 const find = (elements, cb) => {
