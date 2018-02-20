845544d5ba2ceb0e535ce144152c0e22f092c615
Edward Manda <lilkups@yahoo.com>
2018-02-20 15:58:02 -0600

    Edward Manda: JS 1 WIP

--- src/objects.js
+++ src/objects.js
@@ -5,17 +5,22 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const val = Object.values(obj);
+  const key = Object.keys(obj);
+  return Object.mapObject(obj, cb(val, key));
 };
 
 const pairs = (obj) => {
