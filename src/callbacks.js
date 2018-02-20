454a75517876fe1054b1975bd8dfc1c9319cded6
Joshua Goode <jgoode.business@gmail.com>
2018-02-20 16:02:54 -0500

    Corrected error on line 29 of callbacks.js, properly passes all tests

--- src/callbacks.js
+++ src/callbacks.js
@@ -26,7 +26,7 @@ const multiplyNums = (x, y, cb) => {
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  cb(list.contains(item));
+  cb(list.includes(item));
 };
 
 /* STRETCH PROBLEM */
