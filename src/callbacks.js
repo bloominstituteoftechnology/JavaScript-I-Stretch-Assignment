e8a6789ad303bda78ffbffff2e7ce63b9a696663
ASTRO <aquila.strother@asu.edu>
2018-02-20 16:00:37 -0500

    passed tests for all except Stretch Problem

--- src/callbacks.js
+++ src/callbacks.js
@@ -10,19 +10,30 @@ const getLength = (arr, cb) => {
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(y + x);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  const presentArray = () => {    // use arrow functions with callbacks
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === item) {
+        return true;
+      }
+    } return false;
+  };
+  cb(presentArray());
 };
 
 /* STRETCH PROBLEM */
