bc3db5e1be2c987aba4704ed90c6ea6584dd8736
Matthew Tomas <matthewebentomas@gmail.com>
2018-02-20 18:08:39 -0500

    Matthew Tomas Javascript I WIP

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,21 +1,26 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 };
 
 const contains = (item, list, cb) => {
