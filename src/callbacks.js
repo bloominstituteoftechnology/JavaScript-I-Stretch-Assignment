3f5ebded3a3a96c4a1ca94c983c1b8d2c69df10a
Tom Utsey <tom@edgegluer.com>
2018-02-20 17:22:41 -0500

    Thomas utsey still working pushed for attendance record

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,20 +1,24 @@
 const firstItem = (arr, cb) => {
+  cb(arr[0]);
   // firstItem passes the first item of the given array to the callback function.
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
+  cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 };
 
 const sumNums = (x, y, cb) => {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
+  cb(x * y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 };
 
