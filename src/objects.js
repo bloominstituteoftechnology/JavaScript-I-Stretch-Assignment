63271173f66e5ab20550d23b44070822f9fe1099
Nick Coultas <nncoultas@yahoo.com>
2018-02-20 12:51:10 -0800

    Finished Callbacks and Objects

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,28 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  obj.mapObject({ start: 5, end: 12 }, ((val, key) => {
+    return val + 5;
+  }));
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  obj.pairs({ one: 1, two: 2, three: 3 });
 };
 
 /* STRETCH PROBLEMS */
