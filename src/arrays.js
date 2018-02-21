770c607f126723a89eb3d07419921b0b8721fd2b
Robert Hogan <rhogan29@outlook.com>
2018-02-20 21:29:39 -0500

    Robert Hogan. Assignment complete.

--- src/arrays.js
+++ src/arrays.js
@@ -36,9 +36,12 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
-  if (startingValue === undefined) {
-    startingValue = elements[0];
+  let memo = elements.shift();
+  if (startingValue !== undefined) memo = cb(memo, startingValue);
+  for (let i = 0; i < elements.length; i++) {
+    memo = cb(memo, elements[i]);
   }
+  return memo;
 };
 
 const find = (elements, cb) => {
@@ -46,12 +49,25 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filteredArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      filteredArray.push(elements[i]);
+    }
+  }
+  return filteredArray;
 };
 
 /* STRETCH PROBLEM */
