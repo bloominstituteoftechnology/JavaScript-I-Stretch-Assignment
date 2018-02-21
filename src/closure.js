3a9a072462e6d2d5bf40795f121a9b6a1ae715fa
Carlos Andrade <carlos.andrade86@outlook.com>
2018-02-21 16:51:51 -0500

    "Carlos Rob"

--- src/closure.js
+++ src/closure.js
@@ -44,6 +44,13 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (x) => {
+    if (cache.hasOwnProperty(x)) return cache[x];
+    
+    cache[x] = cb(x);
+    return cache[x];
+  };
 };
 
 /* eslint-enable no-unused-vars */
