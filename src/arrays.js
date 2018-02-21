d609020764948ff055e3673abd5b4e0e73254a9e
William <wtpwinberg@gmail.com>
2018-02-20 16:42:57 -0800

    Complete objects

--- src/arrays.js
+++ src/arrays.js
@@ -32,19 +32,19 @@ const map = (elements, cb) => {
   return result;
 };
 
-const reduce = (elements, cb, startingValue) => {
+const reduce = (elements, cb, startingValue = elements.shift()) => {
   // Do NOT use .reduce, to complete this function.
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
-  let result = startingValue !== undefined ? startingValue : elements[0];
+  let memo = startingValue === undefined ? elements[0] : startingValue;
 
   for (let i = 0; i < elements.length; i++) {
-    result = cb(result, elements[i]);
+    memo = cb(memo, elements[i]);
   }
 
-  return result;
+  return memo;
 };
 
 const find = (elements, cb) => {
--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,48 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const result = [];
+  const allKeys = keys(obj);
+
+  for (let i = 0; i < allKeys.length; i++) {
+    if (typeof obj[allKeys[i]] !== 'function') {
+      result.push(obj[allKeys[i]]);
+    }
+  }
+
+  return result;
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const allKeys = keys(obj);
+
+  for (let i = 0; i < allKeys.length; i++) {
+    obj[allKeys[i]] = cb(obj[allKeys[i]]);
+  }
+
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  const result = [];
+  const allKeys = keys(obj);
+
+  for (let i = 0; i < allKeys.length; i++) {
+    result.push([allKeys[i], obj[allKeys[i]]]);
+  }
+
+  return result;
 };
 
 /* STRETCH PROBLEMS */
@@ -29,12 +55,28 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const daPairs = Object.entries(obj);
+  const result = {};
+
+  daPairs.forEach((pair) => {
+    result[pair[1]] = pair[0];
+  });
+
+  return result;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
-  // http://underscorejs.org/#defaults
+  const defKeys = Object.keys(defaultProps);
+
+  defKeys.forEach((key) => {
+    if (obj[key] === undefined) {
+      obj[key] = defaultProps[key];
+    }
+  });
+
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
