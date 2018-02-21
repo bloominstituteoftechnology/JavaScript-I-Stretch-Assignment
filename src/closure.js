adce25a7ff230b20de65b669e3ce59b3b3646c56
Abraham Andujo <foxtara00@gmail.com>
2018-02-21 13:24:50 -0800

    pair programming switch 1

--- src/closure.js
+++ src/closure.js
@@ -16,11 +16,12 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-  let count = 0;
-  return {
-    increment: () => (count++),
-    decrement: () => (count--)
+  const Obj = {
+    count: 0,
+    increment() { return Obj.count++; },
+    decrement() { return Obj.count--; }
   };
+  return Obj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
