74ab4cfa15dc5ef981aafa94421d7adef35da79c
Abraham Andujo <foxtara00@gmail.com>
2018-02-20 17:44:49 -0800

    WIP Abraham Andujo

--- src/objects.js
+++ src/objects.js
@@ -5,12 +5,14 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
@@ -21,6 +23,7 @@ const mapObject = (obj, cb) => {
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 /* STRETCH PROBLEMS */
@@ -29,6 +32,13 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const key = Object.keys(obj);
+  const value = Object.values(obj);
+  const result = {};
+  for (let i = 0; i < key.length; i++) {
+    result[obj[key[i]]] = value[i];
+  }
+  return result;
 };
 
 const defaults = (obj, defaultProps) => {
