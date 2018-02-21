8dda4ac5fc6bb2abeaaa3d3c600a1d7d18b443bc
ASTRO <aquila.strother@asu.edu>
2018-02-21 16:23:34 -0500

    updated objects.js file -- pair programming

--- src/objects.js
+++ src/objects.js
@@ -32,6 +32,13 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const newObj = {};
+  const objKeys = Object.keys(obj);
+  const objVal = Object.values(obj);
+  for (let i = 0; i < objVal.length; i++) {
+    newObj[objVal[i]] = objKeys[i];
+  }
+  return newObj;
 };
 
 const defaults = (obj, defaultProps) => {
