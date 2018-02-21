59f65ce0419533ece84de4e26923327b83bf1475
unknown <adfaris@gmail.com>
2018-02-21 16:10:22 -0500

    Uncommented what Edward commented

--- src/arrays.js
+++ src/arrays.js
@@ -27,7 +27,7 @@ const map = (elements, cb) => {
   for (let i = 0; i < elements.length; i++) {
     arr[i] = cb(elements[i]);
   }
-  // return arr; test case for collaboration tests
+  return arr;
 };
 
 const reduce = (elements, cb, startingValue) => {
