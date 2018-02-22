e5ea090e3326d0b39be35a005791c0e9eff9c2d0
AJ Genung <DragonRydrz@me.com>
2018-02-21 20:32:23 -0500

    completed 100%

--- src/closure.js
+++ src/closure.js
@@ -26,11 +26,14 @@ const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
   let callCount = 0;
-  const invokeCB = () => cb();
-  if (callCount <= n) {
+
+  return (...args) => {
+    if (callCount === n) {
+      return null;
+    }
     callCount++;
-    return cb;
-  }
+    return cb(...args);
+  };
   // return null;
 };
 
@@ -43,6 +46,13 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cacheObj = {};
+  return (num) => {
+    if (num in cacheObj) {
+      return cacheObj[num];
+    }
+    return cacheObj[num] = cb(num);
+  };
 };
 
 /* eslint-enable no-unused-vars */
