36f49880ce85d58c9dadb7a430faf145e9ed4486
RiaClay <sherria.rydyn@gmail.com>
2018-02-21 18:47:45 -0600

    finished stretch problem for closure.

--- src/closure.js
+++ src/closure.js
@@ -51,6 +51,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (object) => {
+    if (Object.prototype.hasOwnProperty.call(cache, object)) {
+      return cache[object];
+    }
+    cache[object] = cb(object);
+    return cache[object];
+  };
 };
 
 /* eslint-enable no-unused-vars */
