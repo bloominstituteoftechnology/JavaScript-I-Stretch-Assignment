765bc1bb1faff51d3c94b623bed51d5edaa14c09
Kenny <khpham24@gmail.com>
2018-02-22 01:13:32 -0700

    final

--- src/arrays.js
+++ src/arrays.js
@@ -14,12 +14,20 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+  cb(elements[i], [i]);
+  }
 };
 
 const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(cb(elements[i], i));
+  }
+  return newArray;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -28,6 +36,10 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let start = 0;
+  const arr = [1, 2, 3, 4];
+  for (let i = 0; i < [1, 2, 3, 4].length; i++) {
+    start += arr[i];
 };
 
 const find = (elements, cb) => {
@@ -35,12 +47,36 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+let find = false;
+  for (let i = 0; i < elements.length; i++){
+    if (cb(elements[i]) === true){
+      find = true;
+      return elements[i];
+    }
+  }
+  if (find === false){
+    return undefined;
+  }
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  let filter = false;
+  const arr = [];
+  const falseArr = [];
+
+  for (let i = 0; i < elements.length; i++){
+    if (cb(elements[i]) === true){
+      arr.push(elements[i]);
+      filter = true;
+    }
+  }
+  if (filter === false){
+    return falseArr;
+  }
+  return arr;
 };
 
 /* STRETCH PROBLEM */
--- src/callbacks.js
+++ src/callbacks.js
@@ -27,7 +27,13 @@ const multiplyNums = (x, y, cb) => {
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
 };
 
 /* STRETCH PROBLEM */
