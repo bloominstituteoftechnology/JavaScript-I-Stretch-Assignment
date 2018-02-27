58693938b046a0c3c767d8ea51753f5854f7b723
Cesar Mejia Leiva <cmejia@gmail.com>
2018-02-27 00:16:45 -0800

    completed callbacks, arrays, objects

--- src/objects.js
+++ src/objects.js
@@ -23,11 +23,14 @@ const mapObject = (obj, cb) => {
   for (let i = 0; i < keyS.length; i++) {
     newObject[keyS[i]] = cb(obj[keyS[i]]);
   }
+  return newObject;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  const duetArray = Object.entries(obj);
+  return duetArray;
 };
 
 /* STRETCH PROBLEMS */
@@ -36,12 +39,24 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const objKeys = Object.keys(obj);
+  const objValues = Object.values(obj);
+  const invObj = {};
+  for (let i = 0; i < objKeys.length; i++) {
+    invObj[objValues[i]] = objKeys[i];
+  }
+  return invObj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  const defaultKeys = Object.keys(defaultProps);
+  for (let i = 0; i < defaultKeys.length; i++) {
+    if (obj[defaultKeys[i]] === undefined) obj[defaultKeys[i]] = defaultProps[defaultKeys[i]];
+  }
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
