8c5275167e8467293c0de5d5da1b93f957d39e1c
Robert Hogan <rhogan29@outlook.com>
2018-03-19 19:07:02 -0400

    Robert Hogan. Mostly completed assignment. 1 assignment before completion. Will attempt stretch goals throughout the night as well.

--- src/arrays.js
+++ src/arrays.js
@@ -14,20 +14,34 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mappedArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    mappedArr.push(cb(elements[i]));
+  }
+  return mappedArr;
 };
 
-const reduce = (elements, cb, startingValue) => {
+const reduce = (elements, cb, startingValue) => {    // need clarification on this!
   // Do NOT use .reduce, to complete this function.
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let memo = elements.shift(); // set memo to the first item (elements.shift();) will make this always the case
+  if (startingValue !== undefined) memo = cb(memo, startingValue); // if the starting value is NOT undefined, then memo is set as the callback function passed memo and the starting value
+  for (let i = 0; i < elements.length; i++) { // iterate through the elements array
+    memo = cb(memo, elements[i]); // memo is set as the callback function passed memo and the index of elements.
+  }
+  return memo;
 };
 
 const find = (elements, cb) => {
@@ -35,12 +49,25 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      return elements[i];
+    }
+  }
+  return 'undefined';
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const passedArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      passedArray.push(elements[i]);
+    }
+  }
+  return passedArray;
 };
 
 /* STRETCH PROBLEM */
