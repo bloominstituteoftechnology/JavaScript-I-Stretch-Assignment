9fa1212c78a557404cb201047f5d6490a634b980
Devon Bacon <devon.bacon@gmail.com>
2018-03-19 14:37:47 -0700

    Completed objects.js with all tests passing

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,45 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+
+  const keysArr = keys(obj);
+  const valsArr = values(obj);
+
+  for (let i = 0; i < keysArr.length; i++) {
+    obj[keysArr[i]] = cb(valsArr[i]);
+  }
+
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+
+  const resultArr = [];
+  const keysArr = keys(obj);
+  const valsArr = values(obj);
+
+  for (let i = 0; i < keysArr.length; i++) {
+    resultArr[i] = [keysArr[i], valsArr[i]];
+  }
+
+  return resultArr;
 };
 
 /* STRETCH PROBLEMS */
