e6d7345d7b0dd1870affe83e370fb9dcf851ec56
Harrison Brock <harrison@harrisonbrock.com>
2018-03-19 10:36:38 -0400

    Arrays.js -- Everything but the stretch problem
    Callbacks.js -- Everything but one test case in last  and stretch

--- src/arrays.js
+++ src/arrays.js
@@ -14,12 +14,22 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let index = 0; index < elements.length; index++) {
+    cb(elements[index], index);
+  }
 };
 
 const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+
+  for (let index = 0; index < elements.length; index++) {
+    const num = cb(elements[index]);
+    arr.push(num);
+  }
+  return arr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -28,6 +38,15 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let index = 0;
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+    index = 1;
+  }
+  for (; index < elements.length; index++) {
+    startingValue = cb(startingValue, elements[index]);
+  }
+  return startingValue;
 };
 
 const find = (elements, cb) => {
@@ -35,12 +54,27 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let index = 0; index < elements.length; index++) {
+    const check = cb(elements[index]);
+    if (check) {
+      return elements[index];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const arr = [];
+  for (let index = 0; index < elements.length; index++) {
+    const check = cb(elements[index]);
+    if (check) {
+      arr.push(elements[index]);
+    }
+  }
+  return arr;
 };
 
 /* STRETCH PROBLEM */
--- src/callbacks.js
+++ src/callbacks.js
@@ -1,26 +1,39 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  const item = arr.pop();
+  cb(item);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const sum = x + y;
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const product = x * y;
+  cb(product);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let founded = false;
+  for (let index = 0; index < list.length; index++) {
+    if (item === list[index]) founded = true;
+  }
+  cb(founded);
 };
 
 /* STRETCH PROBLEM */
@@ -29,6 +42,14 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const temp = [];
+
+  array.forEach((element) => {
+    if (!temp.includes(element)) {
+      temp.push(element);
+    }
+  });
+  cb(temp);
 };
 
 /* eslint-enable */
