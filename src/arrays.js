43e209bf82f6b19e894b9a8887df7e30017212ea
Justin Hong <jhong2009@gmail.com>
2018-03-19 14:14:12 -0700

    upload solutions to arrays exercises

--- src/arrays.js
+++ src/arrays.js
@@ -14,12 +14,22 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+
+  let newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  }
+  return newArr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -28,6 +38,14 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+
+  elements.unshift(startingValue);
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i]) {
+      cb(elements[i]);
+    }
+  }
+
 };
 
 const find = (elements, cb) => {
@@ -35,12 +53,29 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    } else {
+      return undefined;
+    }
+  }
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+
+  const result = [];
+
+  for (let el of elements) {
+    if (cb(el)) {
+      result.push(el);
+    }
+  }
+  return result;
 };
 
 /* STRETCH PROBLEM */
@@ -48,6 +83,7 @@ const filter = (elements, cb) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  
 };
 
 /* eslint-enable no-unused-vars, max-len */
