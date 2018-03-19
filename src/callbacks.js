f2431c7890a2ac8cf7407df0607a6bd2793b622e
cgoez <goez2001@gmail.com>
2018-03-19 13:39:21 -0700

    5 callbacks done

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,21 +1,28 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const result = x + y;
+  cb(result);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const result = x * y;
+  cb(result);
 };
 
 const contains = (item, list, cb) => {
