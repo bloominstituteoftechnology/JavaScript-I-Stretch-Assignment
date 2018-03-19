eee7156e03d3dbd3d297e2f169be4188b35d57c0
Shawn Stewart <s.stewart0220@gmail.com>
2018-03-19 18:28:30 -0500

    Partial completion of JavaScript I.

--- src/arrays.js
+++ src/arrays.js
@@ -14,12 +14,20 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  }
+  return newArr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -28,6 +36,15 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let reduced = '';
+  if (startingValue === undefined) {
+    startingValue = 0;
+  }
+  for (let i = startingValue; i < elements.length; i++) {
+    cb(startingValue, elements[i]);
+    reduced += elements[i];
+  }
+  return reduced;
 };
 
 const find = (elements, cb) => {
@@ -35,12 +52,25 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filtered = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      filtered.push(elements[i]);
+    }
+  }
+  return filtered;
 };
 
 /* STRETCH PROBLEM */
--- src/objects.js
+++ src/objects.js
@@ -5,18 +5,30 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  let objEntries = Object.entries(obj);
+  console.log(Object.entries(obj));
 };
+
+const anObject = {
+  name: 'Shawn',
+  age: 21,
+  likesJavascript: true
+};
+
+mapObject(anObject);
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
