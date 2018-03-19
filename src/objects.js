e60b0561f40d7747d2b16d6e904775f56260ebda
Chris Marsh <cjmarsh725@gmail.com>
2018-03-19 13:37:02 -0700

    Objects main problems.

--- src/objects.js
+++ src/objects.js
@@ -2,25 +2,23 @@
 // Reference http://underscorejs.org/ for examples.
 
 const keys = (obj) => {
-  // Retrieve all the names of the object's properties.
-  // Return the keys as strings in an array.
-  // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
-  // Return all of the values of the object's own properties.
-  // Ignore functions
-  // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const output = {};
+  Object.keys(obj).forEach(x => output[x] = cb(obj[x]));
+  return output;
 };
 
 const pairs = (obj) => {
-  // Convert an object into a list of [key, value] pairs.
-  // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 /* STRETCH PROBLEMS */
