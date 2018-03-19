3a449adf1d4f7162b79e3d3ec898e46014cc0504
kammagik <kammagik@yahoo.com>
2018-03-19 14:38:50 -0700

    last commit

--- src/callbacks.js
+++ src/callbacks.js
@@ -19,7 +19,7 @@ const multiplyNums = (x, y, cb) => {
 };
 
 const contains = (item, list, cb) => {
-  // contains checks if an item is present inside of the given array/list.
+  item.forEach(element => alert(item));// contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 };
 
