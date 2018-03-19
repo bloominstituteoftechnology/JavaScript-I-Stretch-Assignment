2708d3a118cb5599aac77123c2b57bcd47d977ab
Axhon <axhonruiz@gmail.com>
2018-03-19 13:15:21 -1000

    objects done

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,34 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+
+  // always return an array
+  return [...Object.keys(obj)];
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return [...Object.values(obj)];
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const newObj = {};
+  const handleMutation = el => (newObj[el] = cb(obj[el]));
+  keys(obj).map(handleMutation);
+  return newObj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  const objKeys = keys(obj);
+  const objValues = values(obj);
+
+  return objKeys.map((k, i) => [objKeys[i], objValues[i]]);
 };
 
 /* STRETCH PROBLEMS */
@@ -29,12 +41,20 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const copy = {};
+  const objKeys = keys(obj);
+  const objValues = values(obj);
+  objValues.map((k, i) => (copy[k] = objKeys[i]));
+  return copy;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  const vals = values(obj);
+  obj = Object.assign(defaultProps, obj);
+  return obj;
 };
 
 /* eslint-enable no-unused-vars */
@@ -45,5 +65,5 @@ module.exports = {
   mapObject,
   pairs,
   invert,
-  defaults,
+  defaults
 };
