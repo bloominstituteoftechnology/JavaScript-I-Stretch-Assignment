4dba4509d833c8362196e6aa71c5ade92c199f22
SyCoDe <SyCoDe@gmail.com>
2018-03-19 14:33:48 -0600

    finished callbacks, started arrays

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,24 +1,31 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length);
 };
 
 const last = (arr, cb) => {
+  cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 };
 
 const sumNums = (x, y, cb) => {
+  cb(x + y);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 };
 
 const multiplyNums = (x, y, cb) => {
+  cb(x * y);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 };
 
 const contains = (item, list, cb) => {
+  if (list.indexOf(item) >= 0) {
+    cb(true);
+  } cb(false);
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 };
@@ -26,6 +33,13 @@ const contains = (item, list, cb) => {
 /* STRETCH PROBLEM */
 
 const removeDuplicates = (array, cb) => {
+  const arrNew = [array[0]];
+  for (let i = 1; i < array.length; i++) {
+    if (!arrNew.indexOf(array[i]) >= 0) {
+      arrNew.push(array[i]);
+    }
+  }
+  cb(arrNew);
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
