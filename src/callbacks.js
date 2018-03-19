2b43cd36d83370e18392e703fa38db7ea52a9bed
Jared Cuffe <jcuffe@gmail.com>
2018-03-19 12:03:48 -0700

    Callbacks complete, with stretch

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,26 +1,36 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  return cb(arr[0]);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.indexOf(item) !== -1) {
+    return cb(true);
+  }
+
+  return cb(false);
 };
 
 /* STRETCH PROBLEM */
@@ -29,6 +39,14 @@ const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array.indexOf(array[i], i + 1) === -1) {
+      newArr.push(array[i]);
+    }
+  }
+
+  return cb(newArr);
 };
 
 /* eslint-enable */
