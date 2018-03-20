b36b349be5b153b6deb64462ba860a547b2bfd2d
Matthias Bakken <matthias.bakken@gmail.com>
2018-03-20 14:14:08 -0700

    finished objects and closures

--- src/closure.js
+++ src/closure.js
@@ -1,32 +1,59 @@
 // Complete the following functions.
 
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-  // Example: const newCounter = counter();
-  // newCounter(); // 1
-  // newCounter(); // 2
+// Return a function that when invoked increments and returns a counter variable.
+// Example: const newCounter = counter();
+// newCounter(); // 1
+// newCounter(); // 2
+  let count = 0;
+  const counting = () => {
+    count++;
+    return count;
+  };
+  return counting;
 };
 
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+  let count1 = 0;
+  let count2 = 0;
+  const newObj = {
+    increment: () => {
+      count1++;
+      return count1;
+    },
+    decrement: () => {
+      count2--;
+      return count2;
+    }
+  };
+  return newObj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
+// Should return a function that invokes `cb`.
+// The returned function should only allow `cb` to be invoked `n` times.
+  let timesInvoked = 0;
+  return (...args) => {
+    if (timesInvoked === n) {
+      return null;
+    }
+    timesInvoked++;
+    return cb(...args);
+  };
 };
 
 /* STRETCH PROBLEM */
 
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+// Should return a funciton that invokes `cb`.
+// A cache (object) should be kept in closure scope.
+// The cache should keep track of all arguments have been used to invoke this function.
+// If the returned function is invoked with arguments that it has already seen
+// then it should return the cached result and not invoke `cb` again.
+// `cb` should only ever be invoked once for a given set of arguments.
 };
 
 /* eslint-enable no-unused-vars */
--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,43 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const objVal = {};
+  const objEntry = Object.entries(obj);  // [[a: 1], [b: 2], [c: 3]]
+  for (let i = 0; i < objEntry.length; i++) {
+    objEntry[i][1] = cb(objEntry[i][1]);
+    // newObj.objEntry[i] = objEntry[i][1];
+  }
+  // magic to turn our nested arrays into objects
+  objEntry.forEach((a) => {
+    let p = objVal;
+    const v = a.pop();
+    const k = a.reduce((r, b) => {
+      p[r] = p[r] || {};
+      p = p[r];
+      return b;
+    });
+    p[k] = v;
+  });
+  return objVal;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 /* STRETCH PROBLEMS */
