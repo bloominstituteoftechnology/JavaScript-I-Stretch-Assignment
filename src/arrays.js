fd7c83ae78114d870ee89ca8ba75089be876b131
jaivonmassena <jaivonmassena@gmail.com>
2018-03-20 18:58:27 -0400

    completed reduce

--- src/arrays.js
+++ src/arrays.js
@@ -34,12 +34,22 @@ const map = (elements, cb) => {
 
 const reduce = (elements, cb, startingValue) => {
   // Do NOT use .reduce, to complete this function.
-  // Combine all elements into a single value going from left to right.
-  // Elements will be passed one by one into `cb` along with the `startingValue`.
+    // Combine all elements into a single value going from left to right.
+    // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
 
+  let i = 0;
+  if (startingValue === undefined) {
+    startingValue = elements[0];
+    i = 1;
+  }
+  for (; i < elements.length; i++) {
+    startingValue = cb(startingValue, elements[i]);
+  }
+  return startingValue;
 };
+
 const find = (elements, cb) => {
   // Do NOT use .includes, to complete this function.
   // Look through each value in `elements` and pass each element to `cb`.
