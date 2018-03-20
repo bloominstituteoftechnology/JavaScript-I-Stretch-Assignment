5bcc29af9f1ba8016758d78d1430a7e281fe34bc
Justin M <jekm321@gmail.com>
2018-03-19 21:08:53 -0400

    finished objects-- objects, callbacks, and arrays all pass yarn tests

--- src/objects.js
+++ src/objects.js
@@ -16,13 +16,26 @@ const values = (obj) => {
 };
 
 const mapObject = (obj, cb) => {
-  // return Object.mapObject(obj, function(val, key => cb(val)));
+  const prop = Object.keys(obj);
+  const ties = Object.values(obj);
+  const myObj = {};
+  for (let i = 0; i < prop.length; i++) {
+    myObj[prop[i]] = cb(ties[i]);
+  }
+  return myObj;
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
 };
 
 const pairs = (obj) => {
-  return Object.pairs(obj);
+  const prop = Object.keys(obj);
+  const ties = Object.values(obj);
+  const arr = [];
+  for (let i = 0; i < prop.length; i++) {
+    arr.push([prop[i], ties[i]]);
+  }
+  return arr;
+
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
 };
@@ -30,16 +43,29 @@ const pairs = (obj) => {
 /* STRETCH PROBLEMS */
 
 const invert = (obj) => {
-  return Object.invert(obj);
+  const prop = Object.keys(obj);
+  const ties = Object.values(obj);
+  const myObj = {};
+  for (let i = 0; i < prop.length; i++) {
+    myObj[ties[i]] = (prop[i]);
+  }
+  return myObj;
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
 };
 
 const defaults = (obj, defaultProps) => {
-  return Object.defaults(obj, defaultProps);
+  const propDef = Object.keys(defaultProps);
+  const tiesDef = Object.values(defaultProps);
+  for (let i = 0; i < propDef.length; i++) {
+    if (obj[propDef[i]] === undefined) {
+      obj[propDef[i]] = tiesDef[i];
+    }
+  }
+  return obj;
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
-  // Return `obj`.
+  // Return `obj`.r
   // http://underscorejs.org/#defaults
 };
 
