bbd5cf420599e274592582021baae8f85c0d0b8b
JacobLeonlyerla <umasour@yahoo.com>
2018-03-19 21:25:07 -0700

    still cant get test to run fixing errors tho

--- src/arrays.js
+++ src/arrays.js
@@ -8,26 +8,25 @@
   You can use the functions that you have already written to help solve the other problems
 */
 
-//setting up an array as a const
-const eleArray = [3,6,8,5,4];
-  // Do NOT use forEach to complete this function.
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
-  // You should also pass the index into `cb` as the second argument
-  // based off http://underscorejs.org/#each
-  
-  
-  //why!?
-// the  arrow function, set to each
-const each = (elements, cb) =>  {
-  // for itereating over eleArray. 
-for (let index = 0; index < eleArray.length; index++) {
-  //pushing the array element and the array index into cb
- cb(elements[i], i)
+// setting up an array as a const
+const eleArray = [3, 6, 8, 5, 4];
+// Do NOT use forEach to complete this function.
+// Iterates over a list of elements, yielding each in turn to the `cb` function.
+// This only needs to work with arrays.
+// You should also pass the index into `cb` as the second argument
+// based off http://underscorejs.org/#each
+
+//  The arrow function, set to each
+const each = (elements, cb) => {
+  //  for itereating over eleArray
+  for (let i = 0; i < eleArray.length; i++) {
+    //pushing the array element and the array index into cb
+
+    cb(elements[i], i);
   };
 };
 
-//Invoking each with the eleArray
+//  Invoking each with the eleArray
 each(eleArray);
 
 // arrow function set as map
@@ -35,35 +34,33 @@ const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-  
 
-  // setting up ourArray as a const 
+
+  // setting up ourArray as a const
   const ourArray = []
-  //iterating over elements 
-  for (let index = 0; index < elements.length; index++) {
-    //pushing into ourArray the element at index i
+  // iterating over elements
+  for (let i = 0; i < elements.length; i++) {
+  // pushing into ourArray the element at index i
     ourArray.push(elements[i])
   }
-  //returning our new pushed array
+  // returning our new pushed array
   return ourArray
 };
 
 // stting up reduce arrow function
+// Do NOT use .reduce, to complete this function
+// Combine all elements into a single value going from left to right.
+// Elements will be passed one by one into `cb` along with the `startingValue`.
+// `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
+// `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+
 const reduce = (elements, cb, startingValue) => {
-
-  // Do NOT use .reduce, to complete this function.
-  // Combine all elements into a single value going from left to right.
-  // Elements will be passed one by one into `cb` along with the `startingValue`.
-  // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
-  // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
-  
-
   // setting a const of starWars to the starting value
   const starWars = startingValue;
   // initializing total as 0
   let total = 0;
   //Iterating over the elements array
-  for (let index = 0; index < elements.length; index++) {
+  for (let i = 0; i < elements.length; i++) {
     // total equals total plus the current element at the index of i
     total += elements[i];
   }
@@ -75,7 +72,7 @@ const reduce = (elements, cb, startingValue) => {
   // if the starting value(starWars) is defined then push that and the total
   cb(starWars, total)
 };
- // using find as an arrow function
+// using find as an arrow function
 const find = (elements, cb) => {
 
   // Do NOT use .includes, to complete this function.
@@ -83,34 +80,34 @@ const find = (elements, cb) => {
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
 
-  for (let index = 0; index < elements.length; index++) {
+  for (let i = 0; i < elements.length; i++) {
     // if cb at the element index of i = true.
-    if(cb(elements[i]) === true) {
+    if (cb(elements[i]) === true) {
       // then return element at the index of i
       return elements[i]
     }
   }
   // if element at the index of i is equal to false then return undefined
-   return undefined;
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
-  
-  // set a const that is an array to the variable of filteredArray 
-  const filteredArray =[];
 
-  for (let index = 0; index < elements.length; index++) {
+  // set a const that is an array to the variable of filteredArray
+  const filteredArray = [];
+
+  for (let i = 0; i < elements.length; i++) {
     // if cb at the element index of i = true
-    if(cb(elements[i]) === true) {
+    if (cb(elements[i]) === true) {
 
       // push the element at the index of i, into the filteredArray
-       filteredArray.push(elements[i]);
+      filteredArray.push(elements[i]);
     }
-// return the new filteredArray
-return filteredArray;
+    // return the new filteredArray
+    return filteredArray;
 
   }
 
@@ -120,33 +117,37 @@ return filteredArray;
 /* STRETCH PROBLEM */
 
 // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+// Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 
 
 // sets a const with the varibale name of flattenArray to  be equal to an array of arrays
 
-const flattenArray = [[1, 2],[3, 4, 5], [6, 7, 8, 9]]
+const flattenArray = [
+  [1, 2],
+  [3, 4, 5],
+  [6, 7, 8, 9]
+];
 // flatten is the name of the function and fcation is declared using an arrow function
 // also i removed the parenthesize in order to show that they are not needed in the code
-const flatten = elements => {
+const flatten = (elements) => {
   // define a const variable set to myNewArray that holds an empty array
   const myNewArray = [];
   // iterate over an the elements array
-    for (let i = 0; i < elements.length; ++i) {
-      // a nested loop that iterates over the iterating array
-      // using j as the index number and current element i
-      for (let j = 0; j < elements[i].length;  ++j)
+  for (let i = 0; i < elements.length; ++i) {
+    // a nested loop that iterates over the iterating array
+    // using j as the index number and current element i
+   for (let j = 0; j < elements[i].length; ++j){
       // the element at the index of i and j into myNewArray
-        myNewArray.push(elements[i][j]);
-    }
-    //return the newly mapped array(not using .map) 
-   return myNewArray 
+      myNewArray.push(elements[i][j]);
   }
-  // call the flatten function using the flattenArray
-  // setting it to the const variable of value
-  const newflattenedArray = flatten(flattenArray)
-  // log out the new flattened array
-  console.log(newflattenedArray)
+  //return the newly mapped array(not using .map) 
+  return myNewArray;
+}
+// call the flatten function using the flattenArray
+// setting it to the const variable of value
+const newflattenedArray = flatten(flattenArray)
+// log out the new flattened array
+console.log(newflattenedArray)
 
 /* eslint-enable no-unused-vars, max-len */
 
