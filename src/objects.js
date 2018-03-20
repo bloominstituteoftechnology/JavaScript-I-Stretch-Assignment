c613ee79eacbeb91ba9f3a3888d31468be265eec
Velsu <riftmage50@gmail.com>
2018-03-20 22:56:16 +0100

    Completed objects exercises

--- src/objects.js
+++ src/objects.js
@@ -18,7 +18,10 @@ const values = (obj) => {
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-  return Object.values(obj).map(cb);
+  Object.keys(obj).forEach((key) => {
+    obj[key] = cb(obj[key]);
+  });
+  return obj;
 };
 
 const pairs = (obj) => {
