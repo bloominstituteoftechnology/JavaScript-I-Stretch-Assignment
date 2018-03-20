de8f8c0752c8bb17e633d4f03e2ebc4d950c1739
Boredom <macnwandu@gmail.com>
2018-03-20 18:36:50 -0500

    completed

--- src/arrays.js
+++ src/arrays.js
@@ -14,12 +14,20 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
+  for (let i = 0; i <= elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Do NOT use .map, to complete this function.
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  }
+  return newArr;
 };
 
 const reduce = (elements, cb, startingValue) => {
@@ -28,6 +36,16 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let startingIndex = 0;
+  let temp = startingValue;
+  if (startingValue === undefined) {
+    temp = elements[0];
+    startingIndex = 1;
+  }
+  for (let i = startingIndex; i < elements.length; i++) {
+    temp = cb(temp, elements[i]);
+  }
+  return temp;
 };
 
 const find = (elements, cb) => {
@@ -35,12 +53,31 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      return elements[i];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const arr = [];
+  let pass = false;
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      arr.push(elements[i]);
+      pass = true;
+    }
+  }
+  if (pass === false) {
+    return [];
+  } else if (pass === true) {
+    return arr;
+  }
 };
 
 /* STRETCH PROBLEM */
--- src/closure.js
+++ src/closure.js
@@ -5,23 +5,45 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 1;
+  function increment() {
+    return count++;
+  }
+  return increment;
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment() {
+      count++;
+      return count;
+    },
+    decrement() {
+      count--;
+      return count;
+    },
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count === n) return null;
+    count++;
+    return cb(...args);
+  };
 };
 
 /* STRETCH PROBLEM */
 
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
+  // Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
