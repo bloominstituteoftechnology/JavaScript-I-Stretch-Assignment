b2f3ee786c9afe79560bf9eecb848a04c0637586
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-20 18:42:39 -0500

    fixed mapObject() and Pairs() to properly function

--- src/objects.js
+++ src/objects.js
@@ -18,14 +18,15 @@ const values = (obj) => {
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-
+  Object.keys(obj).forEach(key => (obj[key] = cb(obj[key])));
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-  return obj.pairs((pairThing) => {
-    return pairThing.state;
+  return Object.keys(obj).map((key) => {
+    return [key, obj[key]];
   });
 };
 
