ccf0b460b1fa6bd1364047ccf59f17a1c4ef7836
2940cristian <2940cristian@gmail.com>
2018-03-20 15:30:26 -0700

    completed all array exercises except reduce

--- src/arrays.js
+++ src/arrays.js
@@ -14,7 +14,7 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
-  for (let i = 0; i <= elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     cb(elements[i], i);
   }
 };
@@ -50,12 +50,26 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      return elements[i];
+    }
+  } return undefined;
 };
 
 const filter = (elements, cb) => {
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const truthies = [];
+  const falsies = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      truthies.push(elements[i]);
+    }
+  } if (truthies.length > 0) {
+    return truthies;
+  } return falsies;
 };
 
 /* STRETCH PROBLEM */
