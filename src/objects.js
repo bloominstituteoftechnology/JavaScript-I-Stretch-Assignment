8f8532415419c7f1379d7c32c66016a37d8bc582
hilal aissani <hilalaissani@gmail.com>
2018-03-19 22:36:23 -0400

    homework JS-1

--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,31 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  const myKeys = Object.keys(obj);
+  return myKeys;
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  const myValues = Object.values(obj);
+  return myValues;
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const myValues = Object.values(obj);
+  for (let i = 0; i < myValues.length; i++) {
+    return cb(myValues);
+  }
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 /* STRETCH PROBLEMS */
@@ -47,3 +56,4 @@ module.exports = {
   invert,
   defaults,
 };
+
