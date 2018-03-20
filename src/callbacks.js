79280d22b2c89383f2a4c6540aa5fc1db1a36b7d
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-03-19 22:26:03 -0700

    callbacks complete, arrays in progress

--- src/callbacks.js
+++ src/callbacks.js
@@ -1,26 +1,43 @@
 const firstItem = (arr, cb) => {
   // firstItem passes the first item of the given array to the callback function.
+  const item = arr[0];
+  cb(item);
 };
 
 const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
+  const length = arr.length;
+  cb(length);
 };
 
 const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
+  const lastItem = arr[arr.length - 1];
+  cb(lastItem);
 };
 
 const sumNums = (x, y, cb) => {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  // sumNums adds two numbers (x, y) and passes the result to the callback
+  const sum = x + y;
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const prod = x * y;
+  cb(prod);
 };
 
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let test = false;
+  for (let i = 0; i <= list.length; i++) {
+    if (item === list[i]) {
+      test = true;
+    }
+  }
+  cb(test);
 };
 
 /* STRETCH PROBLEM */
@@ -28,7 +45,7 @@ const contains = (item, list, cb) => {
 const removeDuplicates = (array, cb) => {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  // Do not mutate the original array
 };
 
 /* eslint-enable */
