15d77e51f2f423298975664c567900cd045534af
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-20 16:36:43 -0500

    finished closure problems (minus stretch)

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,36 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 1;
+  return function () { return count++; };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  const counterObj = {
+    count: 0,
+  };
+  counterObj.increment = function () {
+    return ++counterObj.count;
+  };
+  counterObj.decrement = function () {
+    return --counterObj.count;
+  };
+  return counterObj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  // let count = 0;
+  const wrappedFunc = () => {
+    count += 1;
+    if (count < n) return cb(...args);
+  };
+  return null;
 };
 
 /* STRETCH PROBLEM */
@@ -27,6 +46,17 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  let storedValue = null;
+  let storedResult = null;
+  return function (newValue) {
+    if (storedValue !== newValue) {
+      storedResult = cb(newValue);
+      storedValue = newValue;
+      return storedResult;
+    }
+    storedValue = newValue;
+    return storedResult;
+  };
 };
 
 /* eslint-enable no-unused-vars */
--- src/objects.js
+++ src/objects.js
@@ -18,13 +18,15 @@ const values = (obj) => {
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-  return Object.mapObject(obj, cb);
+  
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-  return Object.pairs(obj);
+  return obj.pairs((pairThing) => {
+    return pairThing.state;
+  });
 };
 
 /* STRETCH PROBLEMS */
