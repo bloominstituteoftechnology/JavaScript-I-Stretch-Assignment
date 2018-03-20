4ea6c225a0868524eb6ce556828369b340169c25
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-20 16:43:08 -0400

    finished allllll problems

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,41 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return function temp() {
+    count++;
+    return count;
+  };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+    increment() {
+      count++;
+      return 0 + count;
+    },
+    decrement() {
+      count++;
+      return 0 - count;
+    }
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return function (...args) {
+    count++;
+    if (count <= n) {
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 /* STRETCH PROBLEM */
@@ -27,6 +51,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return function (...args) {
+    const argument = args.join(', ');
+    if (Object.keys(cache).includes(argument)) {
+      return cache[argument];
+    }
+    return cache[argument] = cb(argument);
+  };
 };
 
 /* eslint-enable no-unused-vars */
