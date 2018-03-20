b3cfb45f61d0150da14d4fe9d63da1e2f1a207ea
nickbrenn <nbren@tutanota.com>
2018-03-19 20:17:59 -0400

    Completed everything except stretch goals

--- src/arrays.js
+++ src/arrays.js
@@ -36,12 +36,21 @@ const reduce = (elements, cb, startingValue) => {
   // Elements will be passed one by one into `cb` along with the `startingValue`.
   // `startingValue` should be the first argument passed to `cb` and the array element should be the second argument.
   // `startingValue` is the starting value.  If `startingValue` is undefined then make `elements[0]` the initial value.
+  let returnValue = '';
   if (startingValue === undefined) {
-    startingValue = elements[0];
+    returnValue = elements[0];
+
+    for (let i = 1; i < elements.length; i++) {
+      returnValue = cb(returnValue, elements[i]);
+    }
+  } else if (startingValue !== undefined) {
+    returnValue = startingValue;
+
+    for (let i = 0; i < elements.length; i++) {
+      returnValue = cb(returnValue, elements[i]);
+    }
   }
-  for (let i = 0; i < elements.length; i++) {
-    cb(startingValue, elements[i]);
-  }
+  return returnValue;
 };
 
 const find = (elements, cb) => {
--- src/objects.js
+++ src/objects.js
@@ -5,22 +5,31 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const newkeys = Object.keys(obj);
+  const newvalues = Object.values(obj);
+  for (let i = 0; i < newvalues.length; i++) {
+    obj[newkeys[i]] = (cb(newvalues[i]));
+  }
+  return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 /* STRETCH PROBLEMS */
