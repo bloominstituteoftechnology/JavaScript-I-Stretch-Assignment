889ccdda2a8f35d257d205cf6856829a80262699
odax <nr10@albion.edu>
2018-03-21 10:57:55 -0400

    completed all of homeworked except for limitFunctionCallCount in closure. Could not fulfill the last requirement of properly handle arguments in the wrapped function

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,42 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let cont = 0;
+  const counterObject = {
+    increment() {
+      cont++;
+      return cont;
+    },
+    decrement() {
+      cont--;
+      return cont;
+    }
+  };
+  return counterObject;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let numIn = 0;
+  return (anyarg) => {
+    if (numIn < n) {
+      numIn++;
+      return cb(anyarg);
+    }
+    return null;
+  };
 };
 
 /* STRETCH PROBLEM */
