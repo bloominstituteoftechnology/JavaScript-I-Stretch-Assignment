2f8c4c6c5df7110d2f18a3ebfc4c973dca827881
SyCoDe <SyCoDe@gmail.com>
2018-03-20 18:46:12 -0600

    finished objects.js, finished closure.js but working on the stretch

--- src/closure.js
+++ src/closure.js
@@ -1,6 +1,10 @@
 // Complete the following functions.
 
 const counter = () => {
+  let value = 0;
+  return () => {
+    return ++value;
+  };
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
@@ -8,12 +12,29 @@ const counter = () => {
 };
 
 const counterFactory = () => {
+  let value = 0;
+  return {
+    increment: () => {
+      return ++value;
+    },
+    decrement: () => {
+      return --value;
+    }
+  };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
 
 const limitFunctionCallCount = (cb, n) => {
+  let value = 0;
+  return (...args) => {
+    if (value < n) {
+      value++;
+      return cb(...args);
+    }
+    if (value === n) return null;
+  };
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
@@ -21,6 +42,16 @@ const limitFunctionCallCount = (cb, n) => {
 /* STRETCH PROBLEM */
 
 const cacheFunction = (cb) => {
+  const cache = {};
+  return (par1) => {
+    if (cache[par1] !== undefined) {
+      return cache[par1];
+    }
+    if (cache[par1] === undefined) {
+      cache[par1] = cb(par1);
+      return cb(par1);
+    }
+  };
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
