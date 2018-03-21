1e705178b7abd1fade29b72d63f5df00032c3a79
Shawn Stewart <s.stewart0220@gmail.com>
2018-03-20 19:18:57 -0500

    Continuation closure.js

--- src/closure.js
+++ src/closure.js
@@ -16,11 +16,39 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let myCount = 0;
+  const obj = {
+    increment: () => {
+      myCount++;
+      return myCount;
+    },
+    decrement: () => {
+      myCount--;
+      return myCount;
+    }
+  };
+
+  return obj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  // let myCount = 0;
+  // return () => {
+  //   if (myCount < n) {
+  //     myCount++;
+  //     return cb();
+  //   }
+  // };
+  return () => {
+    const myCount = 0;
+    return () => {
+      if (myCount < n) {
+        cb();
+      }
+    };
+  };
 };
 
 /* STRETCH PROBLEM */
