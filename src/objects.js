31249d469f5b602027e59be4733cac893f911c17
K <kkhaag@gmail.com>
2018-10-25 16:30:29 -0600

    readded

---
+++ src/objects.js
@@ -0,0 +1,72 @@
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+
+const keys = (obj) => {
+  // Retrieve all the names of the object's properties.
+  // Return the keys as strings in an array.
+  // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
+};
+
+const values = (obj) => {
+  // Return all of the values of the object's own properties.
+  // Ignore functions
+  // http://underscorejs.org/#values
+  return Object.values(obj);
+};
+
+const mapObject = (obj, cb) => {
+  // Like map for arrays, but for objects. Transform the value of each property in turn.
+  // http://underscorejs.org/#mapObject
+//   const myMap = {};
+//   const key = Object.keys(obj);
+//   for (let i = 0; i < obj.length; i++) {
+
+//     myMap.push(obj[i]);
+//   }
+//   cb(myMap);
+// };
+
+// Object.keys(obj).map(function(key, index) {
+//   obj[key] *= 2;
+// });
+
+// +  const newObj = {};
+// +  const key = Object.keys(obj);
+// +  for (let i = 0; i < key.length; i++) {
+// +    const newValue = cb(obj[key[i]]);
+// +    newObj[key[i]] = newValue;
+// +  }
+// +  return newObj;
+};
+
+const pairs = (obj) => {
+  // Convert an object into a list of [key, value] pairs.
+  // http://underscorejs.org/#pairs
+  return Object.entries(obj);
+};
+
+/* STRETCH PROBLEMS */
+
+const invert = (obj) => {
+  // Returns a copy of the object where the keys have become the values and the values the keys.
+  // Assume that all of the object's values will be unique and string serializable.
+  // http://underscorejs.org/#invert
+};
+
+const defaults = (obj, defaultProps) => {
+  // Fill in undefined properties that match properties on the `defaultProps` parameter object.
+  // Return `obj`.
+  // http://underscorejs.org/#defaults
+};
+
+/* eslint-enable no-unused-vars */
+
+module.exports = {
+  keys,
+  values,
+  mapObject,
+  pairs,
+  invert,
+  defaults,
+};
