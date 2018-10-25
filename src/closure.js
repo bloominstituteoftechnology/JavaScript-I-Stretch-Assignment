dbbd75c7238065e14d671f6de13918790e0bb6eb
K <kkhaag@gmail.com>
2018-10-25 13:13:55 -0600

    Forgot to commit previously

--- src/closure.js
+++ src/closure.js
@@ -4,13 +4,43 @@ const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
-  // newCounter(); // 2
+  // newCounter(); // 2 
+  // in the parent function add a counter variable
+  // make new function
+  // return ++? inside function
+  // return function
+  let myCounter = 0;
+  const myFunction = () => {
+    return ++myCounter;
+  }
+return myFunction;
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  // create an object
+  // create two methods for the object Inc and Dec
+  // create a counter variable in higher scope
+  // increment will increment a var 
+  //   return inc
+  // decrement will dec a var
+  //  return dex
+  // return object
+  let counter = 0;
+  let myObject = {
+    const increment = () => {
+      counter++;
+      return increment;
+    }
+    const decrement = () => {
+      counter--;
+      return decrement;
+    } 
+        
+  }
+  return myObject;
 };
 
 const limitFunctionCallCount = (cb, n) => {
